71a3593628849c7a7f5405a84fa3289e3e0c03e6
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2023-03-30 12:25:08 +0530

    nav added, mobile view ,added all the banners

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -2,24 +2,24 @@ import { Avatar, AvatarGroup, Box, Button, Flex, Text } from '@chakra-ui/react';
 let slate = "#1E293B"
 
 export default function Banner() {
-    return <Box px={"42px"} py={"34px"} w={"737px"} h={"236px"} rounded={"md"} mt={"1.5rem"} bgImage="url('/assets/home/display/money_banner.png')" bgSize={"contain"}>
-        <Text lineHeight={"30px"} color={slate} fontFamily={"Domine"} fontWeight={"700"} fontSize={"26px"}>
+    return <Box px={"2.625rem"} py={"2.125rem"} w={"46.0625rem"} h={"14.75rem"} rounded={"md"} mt={"24px"} bgImage="url('/assets/home/display/money_banner.png')" bgSize={"contain"}>
+        <Text lineHeight={"1.875rem"} color={slate} fontFamily={"Domine"} fontWeight={"700"} fontSize={"1.625rem"}>
             Unlock Your Earning <br />
             Potential on Solana
         </Text>
-        <Text w={"60%"} mt={"7px"} color={slate}>
+        <Text w={"60%"} mt={"0.4375rem"} color={slate}>
             Explore bounties, grants, and job opportunities for developers and non-technical talent alike
         </Text>
-        <Flex mt={"25px"} alignItems={"center"}>
-            <Button bg={"#6366F1"} color={"white"} fontSize={"14px"} px={"36px"} py={"12px"}>
+        <Flex mt={"1.5625rem"} alignItems={"center"}>
+            <Button bg={"#6366F1"} color={"white"} fontSize={"0.875rem"} px={"2.25rem"} py={"0.75rem"}>
                 Sign Up
             </Button>
-            <AvatarGroup size='sm' ml={"46px"} max={3}>
+            <AvatarGroup size='sm' ml={"2.875rem"} max={3}>
                 <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
                 <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
                 <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
             </AvatarGroup>
-            <Text ml={"11px"} fontSize={"14px"}>Join 1,239  others</Text>
+            <Text ml={"0.6875rem"} fontSize={"0.875rem"}>Join 1,239  others</Text>
         </Flex>
     </Box>;
 }
--- components/home/NavHome.tsx
+++ components/home/NavHome.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
-import { Input, Box, Image, Flex, Center, Button } from '@chakra-ui/react';
+import { Input, Box, Image, Flex, Center, Button, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import { ChevronDownIcon } from '@chakra-ui/icons'
 
 function NavHome() {
   const [selected, setselected] = useState('All Opportunties');
   const router = useRouter();
   const [search, setSearch] = useState<string>('');
   return (
-    <Box bg={'FFFFFF'}>
+    <Box bg={'FFFFFF'} >
       <Flex
-        h={'48px'}
-        w={'1400px'}
+        h={'3rem'}
+        px={"1.25rem"}
         bg={'FFFFFF'}
         alignItems={'center'}
         mx={'auto'}
       >
         <Image
-          h={'16.7px'}
+          h={'1.0437rem'}
           alt={'logo'}
           objectFit={'contain'}
           src={'/assets/logo/logo.png'}
@@ -27,9 +28,9 @@ function NavHome() {
               setSearch(e.target.value);
             }}
             placeholder="Search"
-            w={'172px'}
-            h={'32px'}
-            ml={'50px'}
+            w={'10.75rem'}
+            h={'2rem'}
+            ml={'3.125rem'}
           />
           <Button
             onClick={() => {
@@ -48,55 +49,83 @@ function NavHome() {
             Search
           </Button>
         </Flex>
-        <Flex h={'full'} columnGap={'25px'} ml={'20px'}>
+        <Flex h={'full'} columnGap={'1.5625rem'} ml={'1.25rem'}>
+
           {['All Opportunties', 'Bounties', 'Grants', 'Jobs'].map((elm) => {
             return (
               <Center
                 cursor={'pointer'}
-                fontSize={'12px'}
+                fontSize={'0.75rem'}
                 h={'full'}
                 key={elm}
-                onClick={() => {
-                  if (elm.toLocaleLowerCase() == 'all opportunties') {
-                    router.replace(router.asPath.split('?')[0]);
-                    setselected(elm);
-                    return;
-                  }
-                  if (router.asPath.split('?')[1]) {
-                    router.replace(
-                      router.asPath.split('?')[0] +
-                        '?type=' +
-                        elm.toLocaleLowerCase()
-                    );
-                    setselected(elm);
-                    return;
-                  }
-                  router.replace(
-                    router.asPath + '?type=' + elm.toLocaleLowerCase()
-                  );
-                  setselected(elm);
-                }}
-                borderBottom={selected == elm ? '1px solid #6366F1' : ''}
+
+                borderBottom={selected == elm ? '0.0625rem solid #6366F1' : ''}
               >
-                {elm}
+                <Menu>
+                  <MenuButton px={"0.3125rem"} fontSize={'0.75rem'} bg={"transparent"} as={Button} rightIcon={<ChevronDownIcon />}>
+                    {elm}
+                  </MenuButton>
+                  <MenuList zIndex={"500"}>
+                    {
+                      ['All Opportunties',
+                        'Design',
+                        'Growth',
+                        'Content',
+                        'Frontend Development',
+                        'Backend Development',
+                        'Contract Development',].map((option) => {
+                          return (
+                            <MenuItem onClick={() => {
+                              if (elm.toLocaleLowerCase() == 'all opportunties') {
+
+                                if (option.toLocaleLowerCase() == 'all opportunties') {
+                                  router.replace(router.asPath.split('?')[0]);
+                                  setselected(elm);
+                                  return;
+                                }
+
+                                router.replace(router.asPath.split('?')[0] + '?filter=' + option.toLocaleLowerCase());
+                                setselected(elm);
+                                return;
+                              }
+                              if (router.asPath.split('?')[1]) {
+                                router.replace(
+                                  router.asPath.split('?')[0] +
+                                  '?type=' +
+                                  elm.toLocaleLowerCase() + ((option != "All Opportunties") ? '&filter=' + option.toLocaleLowerCase() : '')
+                                );
+                                setselected(elm);
+                                return;
+                              }
+                              router.replace(
+                                router.asPath + '?type=' + elm.toLocaleLowerCase() + ((option != "All Opportunties") ? '&filter=' + option.toLocaleLowerCase() : '')
+                              );
+                              setselected(elm);
+                            }} key={option}>{option}</MenuItem>
+                          )
+                        })
+                    }
+
+                  </MenuList>
+                </Menu>
               </Center>
             );
           })}
         </Flex>
 
-        <Flex ml={'auto'} columnGap={'25px'} alignItems={'center'}>
-          <Center cursor={'pointer'} fontSize={'12px'} h={'full'}>
+        <Flex ml={'auto'} columnGap={'1.5625rem'} alignItems={'center'}>
+          <Center cursor={'pointer'} fontSize={'0.75rem'} h={'full'}>
             Submit A Bounty
           </Center>
-          <Center cursor={'pointer'} fontSize={'12px'} h={'full'}>
+          <Center cursor={'pointer'} fontSize={'0.75rem'} h={'full'}>
             Login
           </Center>
           <Button
             rounded={'md'}
             bg={'#6366F1'}
             color={'white'}
-            fontSize={'12px'}
-            h={'32px'}
+            fontSize={'0.75rem'}
+            h={'2rem'}
           >
             Sign Up
           </Button>
