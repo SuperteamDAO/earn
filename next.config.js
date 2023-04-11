6f62225425c4374443ed9d916bdba0f4d839ed45
krishna <tksumanth1994@gmail.com>
2023-04-11 11:18:25 +0530

    fix: project tooling

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: '',
   reactStrictMode: true,
   swcMinify: true,
   images: {
