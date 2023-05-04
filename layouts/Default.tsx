8b5c0d467c97cb55b78152325f671cf1a023a76d
krishna <tksumanth1994@gmail.com>
2023-05-04 12:52:26 +0530

    fix: default commit

--- layouts/Default.tsx
+++ layouts/Default.tsx
@@ -6,11 +6,16 @@ import Header from '@/components/Header/Header';
 type IDefaultProps = {
   meta: ReactNode;
   children: ReactNode;
+  className?: string;
 };
 
 const Default = (props: IDefaultProps) => {
   return (
-    <div className="min-h-full">
+    <div
+      className={
+        !props.className ? 'min-h-full' : `min-h-full ${props.className}`
+      }
+    >
       {props.meta}
       <Header />
       {props.children}
