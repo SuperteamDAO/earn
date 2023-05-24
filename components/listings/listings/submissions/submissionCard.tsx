14a8e2f31223ad424d7f4ea46c37c05f163e1742
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-05-24 14:13:14 +0530

    fix: changes

--- components/listings/listings/submissions/submissionCard.tsx
+++ components/listings/listings/submissions/submissionCard.tsx
@@ -57,11 +57,15 @@ export const SubmissionCard = ({
   useEffect(() => {
     const fetchImage = async () => {
       if (link) {
-        const { data } = (await axios.post('/api/og', {
-          url: link,
-        })) as { data: Metadata };
-        console.log(data.open_graph.images![0]?.url);
-        setImage(data.open_graph.images![0]?.url ?? '/assets/bg/og.svg');
+        try {
+          const { data } = (await axios.post('/api/og', {
+            url: link,
+          })) as { data: Metadata };
+          setImage(data.open_graph.images![0]?.url ?? '/assets/bg/og.svg');
+        } catch (error) {
+          console.log(error);
+          setImage('/assets/bg/og.svg');
+        }
       }
     };
     fetchImage();
