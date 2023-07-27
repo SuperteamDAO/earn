33111ef17723e6b1468edd0a2e5b1ae7bb7f4292
abhishek <abbhishekk27@gmail.com>
2023-07-28 02:12:11 +0530

    test: resend templates

---
+++ components/emails/welcomeTalent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const WelcomeTalentEmailTemplate = ({ name }: { name: string }) => {
+  return (
+    <div>
+      <p>Hey there {name},</p>
+      <p>
+        Congrats on joining the Talent Network - we&apos;re hyped to have you on
+        board! 🎉
+      </p>
+      <p>
+        Congrats on joining the Talent Network - we&apos;re hyped to have you on
+        board! 🎉
+      </p>
+      <p>
+        We’re still in beta, so if you ever have a question, ideas, or funny
+        memes, don&apos;t hesitate to reply to this email. I read every message
+        😊
+      </p>
+      <p>Best,</p>
+      <p>Kash and the Superteam Earn Crew 🦸‍♀️🦸‍♂️</p>
+    </div>
+  );
+};
