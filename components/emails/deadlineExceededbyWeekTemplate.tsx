695f42b63edb69aa8e46646c952f282fef18c869
Abhishek Ajithkumar <67309069+a20hek@users.noreply.github.com>
2023-08-02 10:53:05 +0530

    update: deadlineExceededbyWeekTemplate.tsx

--- components/emails/deadlineExceededbyWeekTemplate.tsx
+++ components/emails/deadlineExceededbyWeekTemplate.tsx
@@ -19,13 +19,13 @@ export const DeadlineExceededbyWeekTemplate = ({
       <p style={styles.textWithMargin}>
         The deadline for the <strong>{bountyName}</strong> bounty expired a week
         ago. The participants of the bounty would be expecting the results to be
-        out soon — We request you to publish the winners on Earn shortly!
+        out soon — request you to publish the winners on Earn shortly!
       </p>
       <p style={styles.textWithMargin}>
         <a href={link} style={styles.link}>
           Click here
         </a>{' '}
-        to review the submissions:
+        to review the submissions.
       </p>
       <p style={styles.textWithMargin}>Best,</p>
       <p style={styles.text}>The Superteam Earn Crew 🦸&zwj;♀️🦸&zwj;♂️</p>
