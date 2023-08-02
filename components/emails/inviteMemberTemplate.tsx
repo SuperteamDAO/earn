36d5b03eb9b547cce46a41d408e1e087cb7d9a7c
abhishek <abbhishekk27@gmail.com>
2023-08-02 17:49:51 +0530

    refactor: invite email to resend

---
+++ components/emails/inviteMemberTemplate.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { styles } from './styles';
+
+interface TemplateProps {
+  senderName: string;
+  sponsorName: string;
+  link: string;
+}
+
+export const InviteMemberTemplate = ({
+  senderName,
+  sponsorName,
+  link,
+}: TemplateProps) => {
+  return (
+    <div style={styles.container}>
+      <p style={styles.text}>
+        You have been invited to join <strong>{sponsorName}</strong> {''}
+        by {senderName}!
+      </p>
+      <p style={styles.textWithMargin}>
+        <a href={link} style={styles.link}>
+          Click here
+        </a>{' '}
+        to get added as a team member of {sponsorName} on Superteam Earn
+      </p>
+      <p style={styles.text}>Best,&nbsp;</p>
+      <p style={styles.text}>The Superteam Earn Crew 🦸&zwj;♀️🦸&zwj;♂️</p>
+    </div>
+  );
+};
