95efa54c214f101713201d28ef209b2f62cc27df
abhishek <abbhishekk27@gmail.com>
2023-08-21 22:15:36 +0530

    styles: more beutification for walletmultibutton

--- components/modals/Login/ConnectWallet.tsx
+++ components/modals/Login/ConnectWallet.tsx
@@ -54,70 +54,69 @@ export default function ConnectWallet({
           <InfoOutlineIcon ml={1} w={4} h={4} />
         </Tooltip>
       </Flex>
-      {/* {isAndroidDevice ? ( */}
-
-      <Flex
-        justify={'center'}
-        w="100%"
-        mb={1}
-        px={3}
-        py={1}
-        color={'white'}
-        bg={'brand.purple'}
-        border="1px solid"
-        borderColor="brand.slate.100"
-        borderRadius="md"
-        onClick={handleClick}
-      >
-        <WalletMultiButton />
-      </Flex>
-      {/* ) : ( */}
-      {wallets.map((wallet, index) => {
-        const isLoading = loadingWallet === wallet?.adapter?.name;
-        return (
-          <Box
-            key={index}
-            mb={1}
-            px={3}
-            py={1}
-            color={isLoading ? 'brand.slate.300' : 'brand.slate.500'}
-            bg={isLoading ? 'brand.slate.100' : 'white'}
-            border="1px solid"
-            borderColor="brand.slate.100"
-            borderRadius="md"
-            _hover={{
-              bg: isLoading ? 'brand.slate.100' : 'brand.purple',
-              color: isLoading ? 'brand.slate.300' : 'white',
-            }}
-            cursor={isLoading ? 'default' : 'pointer'}
-            onClick={() => connect(wallet)}
-          >
-            <Flex align="center" gap={4} w="100%">
-              <Box
-                alignItems={'center'}
-                justifyContent={'center'}
-                display={'flex'}
-                w="2rem"
-                h="2rem"
-              >
-                <Image
-                  w="70%"
-                  h="70%"
-                  alt={`${wallet?.adapter?.name} Icon`}
-                  src={wallet?.adapter?.icon ?? ''}
-                />
-              </Box>
-              <Flex align="center" gap={2}>
-                <Text ml={2} fontWeight={700}>
-                  {wallet?.adapter?.name ?? ''}
-                </Text>
-                {isLoading && <Spinner color="brand.slate.500" size="xs" />}
+      {isAndroidDevice ? (
+        <Flex
+          justify={'center'}
+          w="100%"
+          mb={1}
+          px={3}
+          py={1}
+          color={'white'}
+          bg={'brand.purple'}
+          border="1px solid"
+          borderColor="brand.slate.100"
+          borderRadius="md"
+          onClick={handleClick}
+        >
+          <WalletMultiButton />
+        </Flex>
+      ) : (
+        wallets.map((wallet, index) => {
+          const isLoading = loadingWallet === wallet?.adapter?.name;
+          return (
+            <Box
+              key={index}
+              mb={1}
+              px={3}
+              py={1}
+              color={isLoading ? 'brand.slate.300' : 'brand.slate.500'}
+              bg={isLoading ? 'brand.slate.100' : 'white'}
+              border="1px solid"
+              borderColor="brand.slate.100"
+              borderRadius="md"
+              _hover={{
+                bg: isLoading ? 'brand.slate.100' : 'brand.purple',
+                color: isLoading ? 'brand.slate.300' : 'white',
+              }}
+              cursor={isLoading ? 'default' : 'pointer'}
+              onClick={() => connect(wallet)}
+            >
+              <Flex align="center" gap={4} w="100%">
+                <Box
+                  alignItems={'center'}
+                  justifyContent={'center'}
+                  display={'flex'}
+                  w="2rem"
+                  h="2rem"
+                >
+                  <Image
+                    w="70%"
+                    h="70%"
+                    alt={`${wallet?.adapter?.name} Icon`}
+                    src={wallet?.adapter?.icon ?? ''}
+                  />
+                </Box>
+                <Flex align="center" gap={2}>
+                  <Text ml={2} fontWeight={700}>
+                    {wallet?.adapter?.name ?? ''}
+                  </Text>
+                  {isLoading && <Spinner color="brand.slate.500" size="xs" />}
+                </Flex>
               </Flex>
-            </Flex>
-          </Box>
-        );
-      })}
-      {/* )} */}
+            </Box>
+          );
+        })
+      )}
     </Box>
   );
 }
