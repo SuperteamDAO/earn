5358210a765f143b5f4e63db57f9d2a9fb83a2af
abhishek <abbhishekk27@gmail.com>
2023-08-23 18:13:24 +0530

    navbar fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -277,13 +277,14 @@ export default function WithSubnavigation() {
           <UserInfo />
         </Stack>
       </Flex>
-
-      <Collapse animateOpacity in={isOpen}>
-        <Flex direction="column" w="96%" mx={'auto'}>
-          <UserInfo isMobile={true} />
-        </Flex>
-        <MobileNav />
-      </Collapse>
+      <Box bg="white">
+        <Collapse animateOpacity in={isOpen}>
+          <Flex direction="column" w="96%" mx={'auto'}>
+            <UserInfo isMobile={true} />
+          </Flex>
+          <MobileNav />
+        </Collapse>
+      </Box>
     </Box>
   );
 }
