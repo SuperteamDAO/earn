845c72c5f3d6823b6840cb6beebf238156ca4e20
abhishek <abbhishekk27@gmail.com>
2023-10-05 00:30:11 +0530

    update: next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://earn-frontend-v2.vercel.app/',
+  siteUrl: 'https://earn.superteam.fun/',
   generateRobotsTxt: true,
 };
