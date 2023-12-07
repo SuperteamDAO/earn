eb63e2327a0d48cfd353bb4425884f03a14750a5
aftabparvez <20273871+aftabparvez@users.noreply.github.com>
2023-12-07 18:57:21 +0530

    excld preview env on custom domains from indexing

--- next.config.js
+++ next.config.js
@@ -1,14 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    dirs: ['.'],
+    dirs: ["."],
   },
   poweredByHeader: false,
   trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
+  },
+  async headers() {
+    const headers = [];
+
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      });
+    }
+
+    return headers;
   },
 };
 
