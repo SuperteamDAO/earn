9e05cd47c3edc97f6d13d3d021a4fdb9e4a0e695
aftabparvez <20273871+aftabparvez@users.noreply.github.com>
2023-12-13 21:58:07 +0530

    seo: where is the robots.txt file generated from on the production site?

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   siteUrl: 'https://earn.superteam.fun/',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/signup', '/dashboard', '/new/sponsor'],
+      },
+    ],
+  },
 };
