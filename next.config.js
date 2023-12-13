c1a162b36e0d4b24f726f0b836dd53ea990f15f4
aftabparvez <20273871+aftabparvez@users.noreply.github.com>
2023-12-14 04:06:06 +0530

    testing optimizePackageImports again

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   experimental: {
     nextScriptWorkers: true,
+    optimizePackageImports: [
+      '@chakra-ui/icons',
+      '@solana-mobile/wallet-adapter-mobile',
+      '@solana/wallet-adapter-base',
+      '@solana/wallet-adapter-react',
+      '@solana/wallet-adapter-react-ui',
+      '@solana/wallet-adapter-wallets',
+      '@solana/web3.js',
+      'mixpanel-browser',
+      'next-seo',
+      'next-sitemap',
+    ],
   },
   eslint: {
     dirs: ['.'],
