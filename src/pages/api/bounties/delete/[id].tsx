6aa061cc9b177dfe3309b0bf932076abe7364aed
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2023-12-14 12:25:25 +0530

    feat: add delete draft (#307)
    * added delete draft
    
    * requested changes
    
    * removed import
    
    * updated menu items
    
    * changed text
    
    * fixed name
    
    * added modal

---
+++ src/pages/api/bounties/delete/[id].tsx
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { prisma } from '@/prisma';
+
+export default async function bountyDelete(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const params = req.query;
+  const id = params.id as string;
+
+  try {
+    const deleteBounty = await prisma.bounties.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!deleteBounty) {
+      return res
+        .status(400)
+        .json({ message: `Bounty with id=${id} not found.` });
+    }
+    if (deleteBounty.status !== 'OPEN' || deleteBounty.isPublished) {
+      return res
+        .status(400)
+        .json({ messsage: 'Only draft bounties can be deleted' });
+    }
+
+    await prisma.bounties.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(200).json({
+      message: `Draft Bounty with id=${id} deleted successfully.`,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error,
+      message: `Error occurred while deleting bounty with id=${id}.`,
+    });
+  }
+}
