389c04f97fcf1c895fb2fe9cbf26abcf3aab8512
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2023-12-18 15:58:58 +0530

    added another # for h1 text

--- src/components/listings/bounty/Bounty.tsx
+++ src/components/listings/bounty/Bounty.tsx
@@ -26,11 +26,11 @@ import { mergeSkills, splitSkills } from '@/utils/skills';
 
 // Pre-fill the bounty description dialog box with headings
 const preFilledHeadings = [
-  '# About the Bounty & Scope',
-  '# Rewards',
-  '# Judging Criteria',
-  '# Submission Requirements',
-  '# Resources',
+  '## About the Bounty & Scope',
+  '## Rewards',
+  '## Judging Criteria',
+  '## Submission Requirements',
+  '## Resources',
 ].join('\n');
 
 interface Props {
