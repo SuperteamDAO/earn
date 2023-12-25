0949ee5888ca4e543ea73182e1710e0b8e46e968
abhishek <abbhishekk27@gmail.com>
2023-12-26 03:19:30 +0530

    fix: toggle winner

--- src/pages/api/submission/toggleWinner.ts
+++ src/pages/api/submission/toggleWinner.ts
@@ -3,44 +3,43 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { prisma } from '@/prisma';
 
-export default async function user(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { id, isWinner, winnerPosition } = req.body;
   try {
-    const result = await prisma.submission.update({
-      where: {
-        id,
-      },
-      data: {
-        isWinner,
-        winnerPosition,
-      },
+    const currentSubmission = await prisma.submission.findUnique({
+      where: { id },
     });
+
+    if (!currentSubmission) {
+      return res.status(404).json({
+        message: `Submission with ID ${id} not found.`,
+      });
+    }
+
+    const result = await prisma.submission.update({
+      where: { id },
+      data: { isWinner, winnerPosition },
+    });
+
     const zapierWebhookUrl = process.env.ZAPIER_SUBMISSION_WEBHOOK!;
     await axios.post(zapierWebhookUrl, result);
-    const bountyId = result.listingId;
-    const updatedBounty = {
-      totalWinnersSelected: {},
-    };
-    if (isWinner) {
-      updatedBounty.totalWinnersSelected = {
-        increment: 1,
-      };
-    } else {
-      updatedBounty.totalWinnersSelected = {
-        decrement: 1,
-      };
+
+    if (currentSubmission.isWinner !== isWinner) {
+      const bountyId = result.listingId;
+      await prisma.bounties.update({
+        where: { id: bountyId },
+        data: {
+          totalWinnersSelected: isWinner ? { increment: 1 } : { decrement: 1 },
+        },
+      });
     }
-    await prisma.bounties.update({
-      where: {
-        id: bountyId,
-      },
-      data: {
-        ...updatedBounty,
-      },
-    });
-    res.status(200).json(result);
+
+    return res.status(200).json(result);
   } catch (error) {
-    res.status(400).json({
+    return res.status(400).json({
       error,
       message: `Error occurred while updating submission ${id}.`,
     });
