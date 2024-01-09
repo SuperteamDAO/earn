1390f379461ccff5de749b21587bb165276b2842
abhishek <abbhishekk27@gmail.com>
2024-01-09 06:26:58 +0530

    update: share icon

--- src/components/misc/shareIcon.tsx
+++ src/components/misc/shareIcon.tsx
@@ -1,7 +1,7 @@
 import { Icon } from '@chakra-ui/react';
 import React from 'react';
-import { IoMdShareAlt } from 'react-icons/io';
+import { IoIosShareAlt } from 'react-icons/io';
 
 export function ShareIcon() {
-  return <Icon as={IoMdShareAlt} ml={{ base: 0, md: -3 }} />;
+  return <Icon as={IoIosShareAlt} ml={{ base: 0, md: -3 }} />;
 }
