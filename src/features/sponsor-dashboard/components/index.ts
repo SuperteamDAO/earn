de1c7d17e32a17962ac6c290677eaa9f1770072a
abhishek <abbhishekk27@gmail.com>
2024-02-13 04:31:52 +0530

    chore: refactor folder structure

--- src/features/sponsor-dashboard/components/index.ts
+++ src/features/sponsor-dashboard/components/index.ts
@@ -1,10 +1,1 @@
-import { SubmissionDetails } from './SubmissionDetails';
-import { SubmissionHeader } from './SubmissionHeader';
-import { SubmissionList } from './SubmissionList';
-import { SubmissionUserDetails } from './SubmissionUserDetails';
-export {
-  SubmissionDetails,
-  SubmissionHeader,
-  SubmissionList,
-  SubmissionUserDetails,
-};
+export * from './Submissions';
