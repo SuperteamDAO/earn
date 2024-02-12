a3667a600050859b40708bd0b27cf9bb0e6e3d65
abhishek <abbhishekk27@gmail.com>
2024-02-12 15:40:49 +0530

    fix: scribes announcement bar flicker

--- src/components/Header/ScribesAnnouncementBar.tsx
+++ src/components/Header/ScribesAnnouncementBar.tsx
@@ -1,29 +1,33 @@
-import { Box, Link, Text, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Link, Text, useMediaQuery } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
 
 export const ScribesAnnouncementBar = () => {
-  const display = useBreakpointValue({ base: 'block', md: 'none' });
-  return (
-    <Box
-      as={NextLink}
-      display={display}
-      w="full"
-      color="white"
-      bgColor={'#a459ff'}
-      href={'/scribes'}
-    >
-      <Text
-        p={3}
-        fontSize={{ base: '11px', md: 'sm' }}
-        fontWeight={500}
-        textAlign="center"
+  const [isSmallerThan800] = useMediaQuery('(max-width: 800px)');
+
+  if (isSmallerThan800) {
+    return (
+      <Box
+        as={NextLink}
+        display={'block'}
+        w="full"
+        color="white"
+        bgColor={'#a459ff'}
+        href={'/scribes'}
       >
-        <Link as={NextLink} textDecoration={'underline'} href="/scribes">
-          Click here
-        </Link>{' '}
-        to check out Scribes — a content hackathon with prizes worth over $100k!
-      </Text>
-    </Box>
-  );
+        <Text
+          p={3}
+          fontSize={{ base: '11px', md: 'sm' }}
+          fontWeight={500}
+          textAlign="center"
+        >
+          <Link as={NextLink} textDecoration={'underline'} href="/scribes">
+            Click here
+          </Link>{' '}
+          to check out Scribes — a content hackathon with prizes worth over
+          $100k!
+        </Text>
+      </Box>
+    );
+  } else return;
 };
