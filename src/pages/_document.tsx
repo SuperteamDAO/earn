5bc9804d35cfc90b5d84971945539364563b6d59
abhishek <abbhishekk27@gmail.com>
2024-02-16 15:24:08 +0530

    chore: add back links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
           crossOrigin="anonymous"
         />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        <link rel="preconnect" href="https://app.posthog.com" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" />
+        <link rel="preconnect" href="https://res.cloudinary.com" />
         <link
           rel="preconnect"
           href="https://app.posthog.com"
