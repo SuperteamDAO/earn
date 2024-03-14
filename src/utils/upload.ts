e83a7f26c0f3901d22aa7990ee5bf08c72876cba
abhishek <abbhishekk27@gmail.com>
2024-03-14 06:57:05 +0530

    fix: typo

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -4,7 +4,10 @@ export async function uploadToCloudinary(image: any) {
   const formData = new FormData();
 
   formData.append('file', image);
-  formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDNARY as string);
+  formData.append(
+    'upload_preset',
+    process.env.NEXT_PUBLIC_CLOUDINARY as string,
+  );
   const post = await axios.post(
     `https://api.cloudinary.com/v1_1/dgvnuwspr/image/upload`,
     formData,
