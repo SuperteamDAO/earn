f57c9b090dca2f073c4d0a21378c542812946cd7
abhishek <abbhishekk27@gmail.com>
2024-03-22 07:29:36 +0530

    feat: surveys

--- src/features/listing-builder/components/CreateListing.tsx
+++ src/features/listing-builder/components/CreateListing.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useReducer, useState } from 'react';
 
 import { ErrorSection } from '@/components/shared/ErrorSection';
+import { SurveyModal } from '@/components/Survey';
 import { type MultiSelectOptions, tokenList } from '@/constants';
 import {
   type Bounty,
@@ -73,6 +74,11 @@ export function CreateListing({
   const [slug, setSlug] = useState<string>('');
 
   const { isOpen, onOpen } = useDisclosure();
+  const {
+    isOpen: isSurveyOpen,
+    onClose: onSurveyClose,
+    onOpen: onSurveyOpen,
+  } = useDisclosure();
 
   const [questions, setQuestions] = useState<Ques[]>(
     editable
@@ -223,6 +229,7 @@ export function CreateListing({
       setSlug(`/${result?.data?.type}/${result?.data?.slug}/`);
       setIsListingPublishing(false);
       onOpen();
+      onSurveyOpen();
     } catch (e) {
       setIsListingPublishing(false);
     }
@@ -339,6 +346,13 @@ export function CreateListing({
               slug={slug}
               isOpen={isOpen}
               onClose={() => {}}
+            />
+          )}
+          {isSurveyOpen && (
+            <SurveyModal
+              isOpen={isSurveyOpen}
+              onClose={onSurveyClose}
+              surveyId="018c674f-7e49-0000-5097-f2affbdddb0d"
             />
           )}
           {steps === 1 && (
