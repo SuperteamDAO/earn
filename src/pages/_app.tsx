7217743b4d8f6bba8b83ca350c747d989e5cf291
abhishek <abbhishekk27@gmail.com>
2024-03-25 14:30:13 +0530

    fix: remove semi colon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -100,7 +100,7 @@ function MyApp({ Component, pageProps }: any) {
   return (
     <>
       <NextTopLoader color={'#6366F1'} showSpinner={false} />
-      <Component {...pageProps} key={router.asPath} />;
+      <Component {...pageProps} key={router.asPath} />
     </>
   );
 }
