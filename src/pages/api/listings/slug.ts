93973c74968974ac1ea61f492da40dce5905370b
Sajal0208 <sajaldewangan@gmail.com>
2024-03-28 19:11:14 +0530

    implemented API for generation of unique slugs

--- src/pages/api/listings/slug.ts
+++ src/pages/api/listings/slug.ts
@@ -1,6 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import slugify from 'slugify';
 
 import { prisma } from '@/prisma';
+
+const checkSlug = async (slug: string): Promise<boolean> => {
+  try {
+    const bounty = await prisma.bounties.findFirst({
+      where: {
+        slug,
+        isActive: true,
+      },
+    });
+
+    if (bounty) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(
+      `Error occurred while fetching bounty with slug=${slug}.`,
+      error,
+    );
+    return false;
+  }
+};
+
+const generateUniqueSlug = async (title: string): Promise<string> => {
+  let slug = slugify(title, { lower: true, strict: true });
+  console.log(slug);
+  let slugExists = await checkSlug(slug);
+  let i = 1;
+
+  while (slugExists) {
+    const newTitle = `${title}-${i}`;
+    slug = slugify(newTitle, { lower: true, strict: true });
+    slugExists = await checkSlug(slug);
+    i += 1;
+  }
+
+  return slug;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -8,15 +47,7 @@ export default async function handler(
 ) {
   const { slug } = req.query;
 
-  const listing = await prisma.bounties.findUnique({
-    where: {
-      slug: slug as string,
-    },
-  });
+  const newSlug = await generateUniqueSlug(slug as string);
 
-  if (listing) {
-    return res.status(409).json({ error: 'Slug already exists' });
-  } else {
-    return res.status(200).json({ message: 'Slug is unique' });
-  }
+  res.status(200).json({ slug: newSlug });
 }
