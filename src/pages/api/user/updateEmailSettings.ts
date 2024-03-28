6b3df7caf8226c055a1daea817d7637daa42d232
abhishek <abbhishekk27@gmail.com>
2024-03-28 07:07:44 +0530

    refactor: remove isSubscribed from emailSettings schema

--- src/pages/api/user/updateEmailSettings.ts
+++ src/pages/api/user/updateEmailSettings.ts
@@ -33,7 +33,6 @@ export default async function handler(
         prisma.emailSettings.create({
           data: {
             userId: userId as string,
-            isSubscribed: true,
             category,
           },
         }),
