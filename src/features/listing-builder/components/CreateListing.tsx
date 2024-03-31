7a8aa9b7f1b5bb99786b91f123942cb5f766f6cf
Abhishek Ajithkumar <67309069+a20hek@users.noreply.github.com>
2024-03-31 20:17:06 +0530

    fix: listing slug ux fixes (#521)

--- src/features/listing-builder/components/CreateListing.tsx
+++ src/features/listing-builder/components/CreateListing.tsx
@@ -133,7 +133,10 @@ export function CreateListing({
           ? `${bounty.title} (2)`
           : bounty?.title) || undefined
       : undefined,
-    slug: editable ? bounty?.slug || undefined : undefined,
+    slug: editable
+      ? (isDuplicating && bounty?.slug ? `${bounty.slug}-2` : bounty?.slug) ||
+        undefined
+      : undefined,
     deadline:
       !isDuplicating && editable && bounty?.deadline
         ? dayjs(bounty?.deadline).format('YYYY-MM-DDTHH:mm') || undefined
@@ -419,6 +422,7 @@ export function CreateListing({
               isDuplicating={isDuplicating}
               isPrivate={isPrivate}
               setIsPrivate={setIsPrivate}
+              publishedAt={bounty?.publishedAt}
             />
           )}
         </FormLayout>
