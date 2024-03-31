9bd28881a9d5a3e55d06cc1298f434559111c9b9
abhishek <abbhishekk27@gmail.com>
2024-03-31 16:03:46 +0530

    styles: add container to submit buttons

--- src/features/listings/components/Submission/SubmissionActionButton.tsx
+++ src/features/listings/components/Submission/SubmissionActionButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip, useDisclosure } from '@chakra-ui/react';
+import { Button, Flex, Tooltip, useDisclosure } from '@chakra-ui/react';
 import axios from 'axios';
 import Image from 'next/image';
 import React, {
@@ -254,29 +254,36 @@ export const SubmissionActionButton = ({
         label={!isUserEligibleByRegion ? regionTooltipLabel : ''}
         rounded="md"
       >
-        <Button
+        <Flex
           pos={{ base: 'fixed', md: 'static' }}
           zIndex={999}
-          bottom={5}
+          bottom={0}
           left="50%"
-          w={{ base: '96%', md: 'full' }}
-          mb={5}
-          bg={buttonBG}
-          _hover={{ bg: buttonBG }}
-          _disabled={{
-            opacity: { base: '96%', md: '70%' },
-          }}
+          w="full"
+          px={{ base: 3, md: 0 }}
+          py={{ base: 4, md: 0 }}
+          bg="white"
           transform={{ base: 'translateX(-50%)', md: 'none' }}
-          pointerEvents={btnPointerEvents}
-          isDisabled={isBtnDisabled}
-          isLoading={isUserSubmissionLoading}
-          loadingText={btnLoadingText}
-          onClick={handleSubmit}
-          size="lg"
-          variant="solid"
         >
-          {buttonText}
-        </Button>
+          <Button
+            w={'full'}
+            mb={{ base: 0, md: 5 }}
+            bg={buttonBG}
+            _hover={{ bg: buttonBG }}
+            _disabled={{
+              opacity: { base: '96%', md: '70%' },
+            }}
+            pointerEvents={btnPointerEvents}
+            isDisabled={isBtnDisabled}
+            isLoading={isUserSubmissionLoading}
+            loadingText={btnLoadingText}
+            onClick={handleSubmit}
+            size="lg"
+            variant="solid"
+          >
+            {buttonText}
+          </Button>
+        </Flex>
       </Tooltip>
     </>
   );
