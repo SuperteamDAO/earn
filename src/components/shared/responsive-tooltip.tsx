ced871974f5cf19ea4043049f289fb1a3a61e784
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2024-05-30 23:44:43 +0530

    chore(leaderboard): added new responsive tooltip in filter row

---
+++ src/components/shared/responsive-tooltip.tsx
@@ -0,0 +1,35 @@
+import {
+  Tooltip as ChakraTooltip,
+  type TooltipProps,
+  useOutsideClick,
+} from '@chakra-ui/react';
+import { useRef, useState } from 'react';
+
+// click to open toolips on mobiles
+
+interface Props extends TooltipProps {
+  children: React.ReactNode;
+}
+
+export const Tooltip = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useOutsideClick({
+    ref: ref,
+    handler: () => setIsOpen(false),
+  });
+
+  return (
+    <ChakraTooltip
+      ref={ref}
+      isOpen={isOpen}
+      label={props.label}
+      onClose={() => setIsOpen(false)}
+      onOpen={() => setIsOpen(true)}
+      {...props}
+    >
+      <button onClick={() => setIsOpen(true)}>{props.children}</button>
+    </ChakraTooltip>
+  );
+};
