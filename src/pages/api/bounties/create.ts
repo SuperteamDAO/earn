34875181380da49ef57226bcac0cc75e8adc28a1
abhishek <abbhishekk27@gmail.com>
2024-06-27 06:37:46 +0530

    chore: add usdValue as avg for ranged listings, fix bug where usdValue shows up as null

--- src/pages/api/bounties/create.ts
+++ src/pages/api/bounties/create.ts
@@ -10,43 +10,54 @@ import { fetchTokenUSDValue } from '@/utils/fetchTokenUSDValue';
 async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
   const userId = req.userId;
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId as string,
-    },
-  });
-
-  if (!user || !user.currentSponsorId) {
-    return res
-      .status(403)
-      .json({ error: 'User does not have a current sponsor.' });
-  }
-
-  const { title, ...data } = req.body;
   try {
-    let usdValue = 0;
-    if (data.isPublished === true && data.publishedAt) {
-      const tokenUsdValue = await fetchTokenUSDValue(
-        data.token,
-        data.publishedAt,
-      );
-      usdValue = tokenUsdValue * data.rewardAmount;
+    const user = await prisma.user.findUnique({
+      where: { id: userId as string },
+    });
+
+    if (!user || !user.currentSponsorId) {
+      return res
+        .status(403)
+        .json({ error: 'User does not have a current sponsor.' });
     }
+
+    const { title, ...data } = req.body;
+    let usdValue = 0;
+
+    if (data.isPublished && data.publishedAt) {
+      try {
+        let amount;
+        if (data.compensationType === 'fixed') {
+          amount = data.rewardAmount;
+        } else if (data.compensationType === 'range') {
+          amount = (data.minRewardAsk + data.maxRewardAsk) / 2;
+        }
+
+        if (amount && data.token) {
+          const tokenUsdValue = await fetchTokenUSDValue(
+            data.token,
+            data.publishedAt,
+          );
+          usdValue = tokenUsdValue * amount;
+        }
+      } catch (error) {
+        console.error('Error calculating USD value:', error);
+      }
+    }
+
     const finalData = {
       sponsorId: user.currentSponsorId,
       title,
       usdValue,
       ...data,
     };
+
     const result = await prisma.bounties.create({
       data: finalData,
-      include: {
-        sponsor: true,
-      },
+      include: { sponsor: true },
     });
 
     const shouldSendEmail = await shouldSendEmailForListing(result);
-
     if (shouldSendEmail) {
       await sendEmailNotification({
         type: 'createListing',
@@ -55,17 +66,18 @@ async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
       });
     }
 
-    try {
-      if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+      try {
         const zapierWebhookUrl = process.env.ZAPIER_BOUNTY_WEBHOOK!;
         await axios.post(zapierWebhookUrl, result);
+      } catch (error) {
+        console.error('Error with Zapier Webhook:', error);
       }
-    } catch (err) {
-      console.log('Error with Zapier Webhook -', err);
     }
+
     return res.status(200).json(result);
   } catch (error: any) {
-    console.error(`User ${userId} unable to create a listing`, error.message);
+    console.error(`User ${userId} unable to create a listing:`, error.message);
     return res.status(400).json({
       error,
       message: 'Error occurred while adding a new bounty.',
--- src/pages/api/bounties/update/[id].ts
+++ src/pages/api/bounties/update/[id].ts
@@ -9,58 +9,59 @@ import { prisma } from '@/prisma';
 import { fetchTokenUSDValue } from '@/utils/fetchTokenUSDValue';
 
 async function bounty(req: NextApiRequestWithUser, res: NextApiResponse) {
-  const params = req.query;
-  const id = params.id as string;
+  const { id } = req.query;
   const updatedData = req.body;
 
   try {
     const userId = req.userId;
 
     const user = await prisma.user.findUnique({
-      where: {
-        id: userId as string,
-      },
+      where: { id: userId as string },
     });
 
-    const currentBounty = await prisma.bounties.findUnique({
-      where: { id },
+    const listingBeforeUpdate = await prisma.bounties.findUnique({
+      where: { id: id as string },
     });
-
-    if (!user) {
-      return res
-        .status(403)
-        .json({ error: 'User does not have a current sponsor.' });
-    }
 
     if (
       !user ||
       !user.currentSponsorId ||
-      currentBounty?.sponsorId !== user.currentSponsorId
+      listingBeforeUpdate?.sponsorId !== user.currentSponsorId
     ) {
       return res
         .status(403)
         .json({ error: 'User does not have a current sponsor.' });
     }
 
-    if (!currentBounty) {
+    if (!listingBeforeUpdate) {
       return res
         .status(404)
         .json({ message: `Bounty with id=${id} not found.` });
     }
 
-    const newRewardsCount = Object.keys(updatedData.rewards || {}).length;
-    const currentTotalWinners = currentBounty.totalWinnersSelected || 0;
+    const {
+      rewards,
+      rewardAmount,
+      token,
+      publishedAt,
+      maxRewardAsk,
+      minRewardAsk,
+      compensationType,
+    } = updatedData;
 
+    const newRewardsCount = Object.keys(rewards || {}).length;
+    const currentTotalWinners = listingBeforeUpdate.totalWinnersSelected || 0;
+
+    // update total winners selected and reset winner positions if necessary
     if (newRewardsCount < currentTotalWinners) {
       updatedData.totalWinnersSelected = newRewardsCount;
-
       const positions = ['first', 'second', 'third', 'fourth', 'fifth'];
       const positionsToReset = positions.slice(newRewardsCount);
 
       for (const position of positionsToReset) {
         await prisma.submission.updateMany({
           where: {
-            listingId: id,
+            listingId: id as string,
             isWinner: true,
             winnerPosition: position,
           },
@@ -72,42 +73,48 @@ async function bounty(req: NextApiRequestWithUser, res: NextApiResponse) {
       }
     }
 
-    let usdValue = currentBounty.usdValue;
-    if (currentBounty.rewardAmount !== updatedData.rewardAmount) {
-      const tokenUsdValue = await fetchTokenUSDValue(
-        updatedData.token,
-        updatedData.publishedAt,
-      );
-      usdValue = tokenUsdValue * updatedData.rewardAmount;
+    let usdValue = 0;
+    let amount;
+    try {
+      if (compensationType === 'fixed') {
+        amount = rewardAmount;
+      } else if (compensationType === 'range') {
+        amount = (maxRewardAsk + minRewardAsk) / 2;
+      }
+      if (token && amount) {
+        const tokenUsdValue = await fetchTokenUSDValue(token, publishedAt);
+        usdValue = tokenUsdValue * amount;
+      }
+    } catch (err) {
+      console.error('Error calculating USD value -', err);
     }
 
     const result = await prisma.bounties.update({
-      where: { id },
+      where: { id: id as string },
       data: {
         ...updatedData,
         usdValue,
       },
     });
 
+    // listing email check
     const shouldSendEmail = await shouldSendEmailForListing(result);
-
-    if (currentBounty?.isPublished === false && shouldSendEmail) {
+    if (listingBeforeUpdate.isPublished === false && shouldSendEmail) {
       await sendEmailNotification({
         type: 'createListing',
-        id,
+        id: id as string,
         triggeredBy: userId,
       });
     }
 
+    // check if deadline has changed and send email if it has
     const deadlineChanged =
-      currentBounty.deadline?.toString() !== result.deadline?.toString();
+      listingBeforeUpdate.deadline?.toString() !== result.deadline?.toString();
     if (deadlineChanged && result.isPublished) {
       const dayjsDeadline = dayjs(result.deadline);
       await prisma.comment.create({
         data: {
-          message: `The deadline for this listing has been updated to ${dayjsDeadline.format(
-            'h:mm A, MMMM D, YYYY (UTC)',
-          )}`,
+          message: `The deadline for this listing has been updated to ${dayjsDeadline.format('h:mm A, MMMM D, YYYY (UTC)')}`,
           listingId: result.id,
           authorId: result.pocId,
           type: 'DEADLINE_EXTENSION',
@@ -115,18 +122,18 @@ async function bounty(req: NextApiRequestWithUser, res: NextApiResponse) {
       });
       await sendEmailNotification({
         type: 'deadlineExtended',
-        id,
+        id: id as string,
         triggeredBy: userId,
       });
     }
 
-    try {
-      if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+      try {
         const zapierWebhookUrl = process.env.ZAPIER_BOUNTY_WEBHOOK!;
         await axios.post(zapierWebhookUrl, result);
+      } catch (err) {
+        console.error('Error with Zapier Webhook -', err);
       }
-    } catch (err) {
-      console.log('Error with Zapier Webhook -', err);
     }
 
     return res.status(200).json(result);
