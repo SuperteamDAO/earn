dd8eb90e392c583d31952122714314afb936bc02
abhishek <abbhishekk27@gmail.com>
2024-07-08 17:05:12 +0530

    fix: edit listing

--- src/pages/dashboard/hackathon/[listing]/edit.tsx
+++ src/pages/dashboard/hackathon/[listing]/edit.tsx
@@ -23,7 +23,7 @@ function EditBounty({ listing }: Props) {
     setIsBountyLoading(true);
     try {
       const bountyDetails = await axios.get(
-        `/api/sponsor-dashboard/${listing}/`,
+        `/api/sponsor-dashboard/${listing}/listing`,
       );
       if (bountyDetails.data.hackathonId !== userInfo?.hackathonId) {
         router.push(`/dashboard/hackathon/`);
