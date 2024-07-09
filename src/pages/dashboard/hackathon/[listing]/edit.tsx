93c43d5c48759f9860392e90a1b1ab78afe69cbc
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2024-07-10 00:48:07 +0530

    chore(olympics): edit url fix

--- src/pages/dashboard/hackathon/[listing]/edit.tsx
+++ src/pages/dashboard/hackathon/[listing]/edit.tsx
@@ -23,7 +23,7 @@ function EditBounty({ listing }: Props) {
     setIsBountyLoading(true);
     try {
       const bountyDetails = await axios.get(
-        `/api/sponsor-dashboard/${listing}?type=hackathon`,
+        `/api/sponsor-dashboard/${listing}/listing?type=hackathon`,
       );
       console.log('bountyDetails', bountyDetails.data);
       if (bountyDetails.data.hackathonId !== userInfo?.hackathonId) {
