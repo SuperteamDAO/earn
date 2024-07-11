fa292e3867ebaf148e0e78522d9a860f2b1527c8
abhishek <abbhishekk27@gmail.com>
2024-07-11 15:35:46 +0530

    fix: sentry setup

---
+++ instrumentation.ts
@@ -0,0 +1,9 @@
+export const register = async (): Promise<void> => {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
+};
