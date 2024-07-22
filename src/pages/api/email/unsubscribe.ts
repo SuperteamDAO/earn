63ca9212ea6ec142c9922603780b1057709e860f
abhishek <abbhishekk27@gmail.com>
2024-07-22 13:08:51 +0530

    chore: update unsubscribe endpoint

--- src/pages/api/email/unsubscribe.ts
+++ src/pages/api/email/unsubscribe.ts
@@ -14,40 +14,7 @@ export default async function handler(
       .status(400)
       .json({ message: 'Token is required and must be a string' });
   }
-
-  if (req.method === 'POST') {
-    try {
-      const tokenRecord = await prisma.unsubscribeToken.findUnique({
-        where: { token },
-      });
-
-      if (!tokenRecord) {
-        return res.status(404).json({ message: 'Invalid token' });
-      }
-
-      const user = await prisma.user.findUnique({
-        where: {
-          email: tokenRecord.email,
-        },
-      });
-
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      await prisma.unsubscribedEmail.create({
-        data: {
-          email: tokenRecord.email,
-          id: user.id,
-        },
-      });
-
-      return res.status(200).send('');
-    } catch (error) {
-      logger.error(error);
-      return res.status(500).send('');
-    }
-  } else if (req.method === 'GET') {
+  if (req.method === 'GET') {
     try {
       const tokenRecord = await prisma.unsubscribeToken.findUnique({
         where: { token },
