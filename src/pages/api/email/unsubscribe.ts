fa977382add3d23e4ed71591271853c1ffdb41b9
abhishek <abbhishekk27@gmail.com>
2024-08-02 01:36:07 +0530

    chore: check with both POST and GET handling

--- src/pages/api/email/unsubscribe.ts
+++ src/pages/api/email/unsubscribe.ts
@@ -7,13 +7,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST']);
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    res.setHeader('Allow', ['GET', 'POST']);
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
 
-  const email = req.body.email || (req.query.email as string);
-  const signature = req.body.signature || (req.query.signature as string);
+  const email =
+    req.method === 'GET'
+      ? (req.query.email as string)
+      : req.body.email || (req.query.email as string);
+  const signature =
+    req.method === 'GET'
+      ? (req.query.signature as string)
+      : req.body.signature || (req.query.signature as string);
 
   if (!email || !signature) {
     return res.status(400).json({ message: 'Missing required parameters' });
@@ -40,7 +46,13 @@ export default async function handler(
       data: { email },
     });
 
-    return res.status(200).json({ message: 'Successfully unsubscribed' });
+    if (req.method === 'GET') {
+      return res
+        .status(200)
+        .send('<h1>You have been successfully unsubscribed</h1>');
+    } else {
+      return res.status(200).json({ message: 'Successfully unsubscribed' });
+    }
   } catch (error) {
     console.error('Error updating subscription status:', error);
     return res.status(500).json({ message: 'Internal server error' });
