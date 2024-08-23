31ebd7b8a6fe7ed9e0a59273aa74d7bc92768af3
abhishek <abbhishekk27@gmail.com>
2024-08-24 04:05:18 +0530

    fix: hydration errors

--- src/features/listings/components/ListingPage/StatusBadge.tsx
+++ src/features/listings/components/ListingPage/StatusBadge.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 
 interface StatusBadgeProps {
@@ -9,10 +9,9 @@ interface StatusBadgeProps {
 
 export const StatusBadge = ({ textColor, text, Icon }: StatusBadgeProps) => {
   return (
-    <Text
-      alignItems="center"
+    <Flex
+      align="center"
       gap={2}
-      display="flex"
       py={1}
       color={textColor}
       fontSize={{ base: 'xs', sm: 'sm' }}
@@ -21,7 +20,7 @@ export const StatusBadge = ({ textColor, text, Icon }: StatusBadgeProps) => {
       rounded={'full'}
     >
       {Icon}
-      {text}
-    </Text>
+      <Text>{text}</Text>
+    </Flex>
   );
 };
