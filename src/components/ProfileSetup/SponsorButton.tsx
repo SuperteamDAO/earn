81e445bdc3a4d6de198a68e99a0399cbb166269a
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2024-08-26 16:40:21 +0530

    feat(onboarding): redesigned /new page

--- src/components/ProfileSetup/SponsorButton.tsx
+++ src/components/ProfileSetup/SponsorButton.tsx
@@ -1,39 +1,14 @@
 import { Alert, AlertIcon, Button } from '@chakra-ui/react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { BsBriefcaseFill } from 'react-icons/bs';
 
-import { SponsorStore } from '@/store/sponsor';
-import { useUser } from '@/store/user';
-
-export function SponsorButton() {
-  const router = useRouter();
-  const { setCurrentSponsor } = SponsorStore();
-  const { user } = useUser();
-  const [isLoading, setIsLoading] = useState(false);
-  const [showMessage, setShowMessage] = useState(false);
-
-  const checkSponsor = async () => {
-    if (!user || !user?.id) {
-      setShowMessage(true);
-    } else {
-      setShowMessage(false);
-      setIsLoading(true);
-      try {
-        const sponsors = await axios.get('/api/user-sponsors');
-        if (sponsors?.data?.length) {
-          setCurrentSponsor(sponsors?.data[0]?.sponsor);
-          router.push('/new/listing');
-        } else {
-          router.push('/new/sponsor');
-        }
-      } catch (error) {
-        setIsLoading(false);
-      }
-    }
-  };
-
+export function SponsorButton({
+  showMessage,
+  isLoading,
+  checkSponsor,
+}: {
+  showMessage?: boolean;
+  isLoading?: boolean;
+  checkSponsor: () => void;
+}) {
   return (
     <>
       {!!showMessage && (
@@ -46,15 +21,14 @@ export function SponsorButton() {
         w={'full'}
         h={12}
         color={'white'}
-        fontSize={'0.9rem'}
-        bg={'#6562FF'}
-        _hover={{ bg: '#6562FF' }}
+        bg={'brand.slate.900'}
+        _hover={{ bg: 'brand.slate.700' }}
         isLoading={!!isLoading}
-        leftIcon={<BsBriefcaseFill />}
         loadingText="Redirecting..."
         onClick={() => checkSponsor()}
+        rounded="4px"
       >
-        Make Your Sponsor Profile
+        Continue as a sponsor {'->'}
       </Button>
     </>
   );
--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -1,12 +1,33 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Center,
+  Divider,
+  Flex,
+  Icon,
+  Image,
+  Text,
+} from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { type GetServerSideProps } from 'next';
-import React from 'react';
+import NextImage from 'next/image';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { MdCheck } from 'react-icons/md';
 
 import { SponsorButton } from '@/components/ProfileSetup/SponsorButton';
 import { TalentButton } from '@/components/ProfileSetup/TalentButton';
+import { totalsQuery } from '@/features/home';
 import { Default } from '@/layouts/Default';
 import { Meta } from '@/layouts/Meta';
+import Tensor from '@/public/assets/company-logos/tensor.svg';
+import Jupiter from '@/public/assets/landingsponsor/sponsors/jupiter.png';
+import Solflare from '@/public/assets/landingsponsor/sponsors/solflare.png';
+import Squads from '@/public/assets/landingsponsor/sponsors/squads.png';
+import { SponsorStore } from '@/store/sponsor';
+import { useUser } from '@/store/user';
 import { getURL } from '@/utils/validUrl';
 
 export default function NewProfilePage({
@@ -14,6 +35,68 @@ export default function NewProfilePage({
 }: {
   showTalentProfile: boolean;
 }) {
+  const avatars = [
+    {
+      name: 'Abhishkek',
+      src: '/assets/pfps/t1.png',
+    },
+    {
+      name: 'Pratik',
+      src: '/assets/pfps/md2.png',
+    },
+    {
+      name: 'Yash',
+      src: '/assets/pfps/fff1.png',
+    },
+  ];
+
+  const { data: totals } = useQuery(totalsQuery);
+
+  const router = useRouter();
+  const { user } = useUser();
+  const [isTalentLoading, setIsTalentLoading] = useState(false);
+  const [showTalentMessage, setShowTalentMessage] = useState(false);
+  const { setCurrentSponsor } = SponsorStore();
+  const [isSponsorLoading, setIsSponsorLoading] = useState(false);
+  const [showSponsorMessage, setShowSponsorMessage] = useState(false);
+
+  const checkTalent = async () => {
+    if (!user || !user?.id) {
+      setShowTalentMessage(true);
+    } else {
+      setShowTalentMessage(false);
+      setIsTalentLoading(true);
+      try {
+        if (!user?.isTalentFilled) {
+          router.push('/new/talent');
+        } else {
+          router.push('/');
+        }
+      } catch (error) {
+        setIsTalentLoading(false);
+      }
+    }
+  };
+
+  const checkSponsor = async () => {
+    if (!user || !user?.id) {
+      setShowSponsorMessage(true);
+    } else {
+      setShowSponsorMessage(false);
+      setIsSponsorLoading(true);
+      try {
+        const sponsors = await axios.get('/api/user-sponsors');
+        if (sponsors?.data?.length) {
+          setCurrentSponsor(sponsors?.data[0]?.sponsor);
+          router.push('/dashboard/listings?open=1');
+        } else {
+          router.push('/new/sponsor');
+        }
+      } catch (error) {
+        setIsSponsorLoading(false);
+      }
+    }
+  };
   return (
     <Default
       meta={
@@ -26,194 +109,215 @@ export default function NewProfilePage({
     >
       <Box
         pos={'relative'}
-        justifyContent={'center'}
         display={'flex'}
-        h={'100vh'}
+        maxW="52rem"
+        h={{ md: '100vh' }}
+        mx="auto"
         fontFamily="var(--font-sans)"
       >
-        <Box
-          pos={'absolute'}
-          display={{ base: 'none', md: 'block' }}
-          w={'full'}
-          h={'20rem'}
-          bgImage={`url(/assets/bg/newbanner.svg)`}
-        ></Box>
         <Flex
-          pos={'relative'}
-          align={'center'}
-          justify={{ base: 'start', lg: 'center' }}
+          pos={{ base: 'static', md: 'relative' }}
+          top={{ base: 0, md: '10vh' }}
           direction={{ base: 'column', lg: 'row' }}
-          gap={{ base: '3rem', lg: '6rem' }}
+          gap={{ base: '4rem', md: '2rem' }}
           px={{ base: 4, lg: 0 }}
           py={{ base: 6, lg: 0 }}
         >
-          <Flex
-            direction={'column'}
-            w={{ base: 'full', lg: '24rem' }}
-            bg={'white'}
-            borderRadius={'7px'}
-            shadow={'0px 4px 11px rgba(0, 0, 0, 0.08)'}
-          >
-            <Box
-              alignItems={'center'}
-              justifyContent={'center'}
-              display={'flex'}
-              w={'full'}
-              py={16}
-              bg={'rgba(101, 98, 255, 0.12)'}
-            >
-              <Image
-                h={14}
-                alt={'suitcase icon'}
-                src={'/assets/icons/suitcase.svg'}
-              />
-            </Box>
-            <Box p={4}>
-              <Text color={'black'} fontSize={'1rem'} fontWeight={600}>
-                Looking for Talent?
-              </Text>
-              <Text
-                color={'brand.slate.500'}
-                fontSize={'0.9rem'}
-                fontWeight={400}
-              >
-                List a bounty, or grant for your project and find your next
-                contributor..
-              </Text>
-              <Box flexDir={'column'} gap={5} display={'flex'} my={6}>
-                <Flex align={'center'} gap={2}>
-                  <Image
-                    w={4}
-                    alt={'tick'}
-                    src={'/assets/icons/purple-tick.svg'}
-                  />
-                  <Text
-                    color={'brand.slate.500'}
-                    fontSize={'0.9rem'}
-                    fontWeight={400}
-                  >
-                    Get in front of 1,000+ weekly visitors
-                  </Text>
-                </Flex>
-                <Flex align={'center'} justify={'start'} gap={2}>
-                  <Box w={5}>
-                    <Image
-                      w={4}
-                      alt={'tick'}
-                      src={'/assets/icons/purple-tick.svg'}
-                    />
-                  </Box>
-                  <Text
-                    color={'brand.slate.500'}
-                    fontSize={'0.9rem'}
-                    fontWeight={400}
-                  >
-                    Instant listing creation through templates
-                  </Text>
-                </Flex>
-                <Flex align={'center'} gap={2}>
-                  <Image
-                    w={4}
-                    alt={'tick'}
-                    src={'/assets/icons/purple-tick.svg'}
-                  />
-                  <Text
-                    color={'brand.slate.500'}
-                    fontSize={'0.9rem'}
-                    fontWeight={400}
-                  >
-                    100% free
-                  </Text>
-                </Flex>
-              </Box>
-              <SponsorButton />
-            </Box>
-          </Flex>
-
           {showTalentProfile && (
             <Flex
               direction={'column'}
-              w={{ base: 'full', lg: '24rem' }}
+              gap={9}
+              w="full"
+              cursor="pointer"
+              onClick={checkTalent}
+            >
+              <Flex direction={'column'} gap={1.5}>
+                <Text color="brand.slate.900" fontSize={'2xl'} fontWeight={600}>
+                  Want to Earn?
+                </Text>
+                <Text
+                  color="brand.slate.500"
+                  fontSize={'1.125rem'}
+                  lineHeight={'21.78px'}
+                  letterSpacing="-0.2px"
+                >
+                  Create a profile to start submitting, and get notified on new
+                  work opportunities
+                </Text>
+              </Flex>
+
+              <Flex
+                direction={'column'}
+                gap={4}
+                overflow="hidden"
+                bg={'white'}
+                borderRadius={'7px'}
+              >
+                <Box
+                  pos="relative"
+                  alignItems={'center'}
+                  justifyContent={'center'}
+                  display={'flex'}
+                  w={'full'}
+                >
+                  <NextImage
+                    width={704}
+                    height={328}
+                    style={{ width: '100%' }}
+                    alt={'user icon'}
+                    src={'/assets/onboarding/talent-banner.webp'}
+                  />
+                  <Box
+                    pos="absolute"
+                    top={0}
+                    left={0}
+                    w="full"
+                    h="full"
+                    bg="#A78BFA"
+                    mixBlendMode={'overlay'}
+                  />
+                </Box>
+                <Box flexDir={'column'} gap={5} display={'flex'} px={4}>
+                  <BulletPoint type="TALENT">
+                    Contribute to top Solana projects
+                  </BulletPoint>
+                  <BulletPoint type="TALENT">
+                    Build your web3 resume
+                  </BulletPoint>
+                  <BulletPoint type="TALENT">Get paid in crypto</BulletPoint>
+                </Box>
+                <Divider borderColor="brand.slate.300" />
+                <Box px={4} pb={4}>
+                  <TalentButton
+                    showMessage={showTalentMessage}
+                    isLoading={isTalentLoading}
+                    checkTalent={checkTalent}
+                  />
+                </Box>
+              </Flex>
+              <Flex align="center" gap={6} mx="auto" mt={-3}>
+                <AvatarGroup max={3} size={'xs'}>
+                  {avatars.map((avatar, index) => (
+                    <Avatar
+                      key={index}
+                      pos="relative"
+                      borderWidth={'0px'}
+                      name={avatar.name}
+                      src={avatar.src}
+                    />
+                  ))}
+                </AvatarGroup>
+                {totals?.totalUsers !== null && (
+                  <Text pos="relative" color="brand.slate.500" fontSize="sm">
+                    Join {totals?.totalUsers?.toLocaleString()}+ others
+                  </Text>
+                )}
+              </Flex>
+            </Flex>
+          )}
+          <Flex
+            direction={'column'}
+            gap={9}
+            w="full"
+            cursor="pointer"
+            onClick={checkSponsor}
+          >
+            <Flex direction={'column'} gap={1.5}>
+              <Text color="brand.slate.900" fontSize={'2xl'} fontWeight={600}>
+                Find Contributors
+              </Text>
+              <Text
+                color="brand.slate.500"
+                fontSize={'1.125rem'}
+                lineHeight={'21.78px'}
+                letterSpacing="-0.2px"
+              >
+                List a bounty or freelance gig for your project and find your
+                next contributor
+              </Text>
+            </Flex>
+            <Flex
+              direction={'column'}
+              gap={4}
+              overflow="hidden"
               bg={'white'}
               borderRadius={'7px'}
-              shadow={'0px 4px 11px rgba(0, 0, 0, 0.08)'}
             >
               <Box
+                pos="relative"
                 alignItems={'center'}
                 justifyContent={'center'}
                 display={'flex'}
                 w={'full'}
-                py={16}
-                bg={
-                  'radial-gradient(50% 50% at 50% 50%, rgba(101, 98, 255, 0.12) 0%, rgba(98, 255, 246, 0.12) 100%)'
-                }
               >
-                <Image
-                  h={14}
+                <NextImage
+                  width={704}
+                  height={328}
+                  style={{ width: '100%' }}
                   alt={'user icon'}
-                  src={'/assets/icons/userIcon.svg'}
+                  src={'/assets/onboarding/sponsor-banner.webp'}
+                />
+                <Box
+                  pos="absolute"
+                  top={0}
+                  left={0}
+                  w="full"
+                  h="full"
+                  bg="#10B981"
+                  mixBlendMode={'overlay'}
                 />
               </Box>
-              <Box p={4}>
-                <Text color={'black'} fontSize={'1rem'} fontWeight={600}>
-                  Looking to Earn?
-                </Text>
-                <Text
-                  color={'brand.slate.500'}
-                  fontSize={'0.9rem'}
-                  fontWeight={400}
-                >
-                  Create a profile to get notified when new earning
-                  opportunities get posted.
-                </Text>
-                <Box flexDir={'column'} gap={5} display={'flex'} my={6}>
-                  <Flex align={'center'} gap={2}>
-                    <Image
-                      w={4}
-                      alt={'tick'}
-                      src={'/assets/icons/purple-tick.svg'}
-                    />
-                    <Text
-                      color={'brand.slate.500'}
-                      fontSize={'0.9rem'}
-                      fontWeight={400}
-                    >
-                      Start contributing to top crypto projects
-                    </Text>
-                  </Flex>
-                  <Flex align={'center'} gap={2}>
-                    <Image
-                      w={4}
-                      alt={'tick'}
-                      src={'/assets/icons/purple-tick.svg'}
-                    />
-                    <Text
-                      color={'brand.slate.500'}
-                      fontSize={'0.9rem'}
-                      fontWeight={400}
-                    >
-                      Build your on-chain resume
-                    </Text>
-                  </Flex>
-                  <Flex align={'center'} gap={2}>
-                    <Image
-                      w={4}
-                      alt={'tick'}
-                      src={'/assets/icons/purple-tick.svg'}
-                    />
-                    <Text
-                      color={'brand.slate.500'}
-                      fontSize={'0.9rem'}
-                      fontWeight={400}
-                    >
-                      Get paid in crypto
-                    </Text>
-                  </Flex>
-                </Box>
-                <TalentButton />
+              <Box flexDir={'column'} gap={5} display={'flex'} px={4}>
+                <BulletPoint type="SPONSOR">
+                  Get in front of 10,000 weekly visitors
+                </BulletPoint>
+                <BulletPoint type="SPONSOR">
+                  20+ templates to choose from
+                </BulletPoint>
+                <BulletPoint type="SPONSOR">100% free</BulletPoint>
+              </Box>
+              <Divider borderColor="brand.slate.300" />
+              <Box px={4} pb={4}>
+                <SponsorButton
+                  isLoading={isSponsorLoading}
+                  showMessage={showSponsorMessage}
+                  checkSponsor={checkSponsor}
+                />
               </Box>
             </Flex>
-          )}
+            <Flex align="center" justify="space-between" gap={3} mt={-3} px={3}>
+              <Image
+                as={NextImage}
+                h={'20px'}
+                objectFit={'contain'}
+                alt="Jupiter Icon"
+                src={Jupiter as unknown as string}
+              />
+              <Image
+                as={NextImage}
+                h={'34px'}
+                objectFit={'contain'}
+                alt="Solflare Icon"
+                src={Solflare as unknown as string}
+              />
+              <Image
+                as={NextImage}
+                display={{ base: 'none', md: 'block' }}
+                h={'18px'}
+                objectFit={'contain'}
+                alt="Squads Icon"
+                src={Squads as unknown as string}
+              />
+              <Image
+                as={NextImage}
+                w={'28px'}
+                h={'28px'}
+                objectFit={'contain'}
+                alt="Tensor Icon"
+                src={Tensor as unknown as string}
+              />
+            </Flex>
+          </Flex>
         </Flex>
       </Box>
     </Default>
@@ -251,4 +355,38 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       showTalentProfile,
     },
   };
+};
+
+const TickIcon = ({ type }: { type: 'TALENT' | 'SPONSOR' }) => {
+  return (
+    <Center
+      p={'3px'}
+      bg={type === 'TALENT' ? '#EEF2FF' : '#ECFDF5'}
+      rounded="full"
+    >
+      <Icon
+        as={MdCheck}
+        w="0.8rem"
+        h="0.8rem"
+        color={type === 'TALENT' ? '#4F46E5' : '#059669'}
+      />
+    </Center>
+  );
+};
+
+const BulletPoint = ({
+  type,
+  children,
+}: {
+  type: 'TALENT' | 'SPONSOR';
+  children: React.ReactNode;
+}) => {
+  return (
+    <Flex align={'center'} gap={4}>
+      <TickIcon type={type} />
+      <Text color={'brand.slate.500'} fontWeight={400}>
+        {children}
+      </Text>
+    </Flex>
+  );
 };
