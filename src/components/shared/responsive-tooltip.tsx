e992f97471bcd48ad3aaff01250f669fe216e4fd
abhishek <abbhishekk27@gmail.com>
2024-09-13 09:02:27 +0530

    fix: social link stuff

--- src/components/shared/responsive-tooltip.tsx
+++ src/components/shared/responsive-tooltip.tsx
@@ -5,8 +5,6 @@ import {
 } from '@chakra-ui/react';
 import { useRef, useState } from 'react';
 
-// click to open toolips on mobiles
-
 interface Props extends TooltipProps {
   children: React.ReactNode;
 }
@@ -20,13 +18,16 @@ export const Tooltip = (props: Props) => {
     handler: () => setIsOpen(false),
   });
 
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <ChakraTooltip
       ref={ref}
       isOpen={isOpen}
       label={props.label}
       onClose={() => setIsOpen(false)}
-      onOpen={() => setIsOpen(true)}
       {...props}
     >
       <button
@@ -36,7 +37,7 @@ export const Tooltip = (props: Props) => {
           justifyContent: 'center',
           width: '100%',
         }}
-        onClick={() => setIsOpen(true)}
+        onClick={handleToggle}
       >
         {props.children}
       </button>
