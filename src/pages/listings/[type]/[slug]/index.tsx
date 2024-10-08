6d0828f5433a411bcada2e82e6796374df0355c5
abhishek <abbhishekk27@gmail.com>
2024-10-08 15:59:33 +0530

    chore: synternet redirect

--- src/pages/listings/[type]/[slug]/index.tsx
+++ src/pages/listings/[type]/[slug]/index.tsx
@@ -51,6 +51,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
+  if (slug === 'synthernet-radar' && !host.includes('earn.superteam.fun')) {
+    return {
+      redirect: {
+        destination: `https://earn.superteam.fun/listings/hackathon/synternet-radar/`,
+        permanent: false,
+      },
+    };
+  }
+
   if (slug === '100xdevs-solana-mini-hackathon-1' && type === 'project') {
     return {
       redirect: {
