ed70deec4f86b2e9f2a2acc432b8113e13d69ba7
Peter Salomonsen <petersalomonsen@runbox.com>
2024-11-01 20:01:40 +0000

    run migration in codespace

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -7,3 +7,5 @@ sudo service mysql start
 sudo mysql -e "create database earn";
 sudo mysql -e "create user earn@localhost identified by 'earn'";
 sudo mysql -e "grant all privileges on *.* to earn@localhost";
+
+npx prisma migrate dev --name init && npx prisma generate
