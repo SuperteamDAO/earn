34f34cdc28401bd25469afc0e6d86bfddfc266c0
Peter Salomonsen <petersalomonsen@runbox.com>
2024-11-02 18:20:05 +0000

    more files to docker ignore

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@ service mysql start
 export DATABASE_URL=mysql://earn:earn@127.0.0.1:3306/earn
 
 # Check if the database exists, and if not, create it
-if ! mysql -u root -e 'USE earn'; then
+if ! mysql -e 'USE earn'; then
   mysql -e "CREATE DATABASE earn"
   mysql -e "CREATE USER 'earn'@'localhost' IDENTIFIED BY 'earn'"
   mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'earn'@'localhost'"
