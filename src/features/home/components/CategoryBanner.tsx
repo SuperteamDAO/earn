18b4db8dd1c2be5930a20b0364e094ab049b492e
liushooter <shooter.hero@gmail.com>
2024-11-13 12:08:19 +0700

    up

--- src/features/home/components/CategoryBanner.tsx
+++ src/features/home/components/CategoryBanner.tsx
@@ -65,7 +65,7 @@ export function CategoryBanner({ category }: { category: CategoryTypes }) {
             width: '100%',
             height: '100%',
             objectFit: 'cover',
-            objectPosition: 'top',
+            objectPosition: 'top center',
           }}
         />
         <Box
