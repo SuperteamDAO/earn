15a72f097693ec07ae67b4a0cc3353bd2423085e
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2024-11-22 17:18:48 +0530

    chore: unpublish api

---
+++ src/pages/api/listings/unpublish/[id].ts
@@ -0,0 +1,95 @@
+import { type NextApiResponse } from 'next';
+
+import {
+  checkListingSponsorAuth,
+  type NextApiRequestWithSponsor,
+  withSponsorAuth,
+} from '@/features/auth';
+import earncognitoClient from '@/lib/earncognitoClient';
+import logger from '@/lib/logger';
+import { prisma } from '@/prisma';
+import { safeStringify } from '@/utils';
+
+async function handler(req: NextApiRequestWithSponsor, res: NextApiResponse) {
+  const id = req.query.id as string;
+  const userId = req.userId;
+  const userSponsorId = req.userSponsorId;
+
+  if (!userId) {
+    logger.warn('Invalid token: User Id is missing');
+    return res.status(400).json({ error: 'Invalid token' });
+  }
+
+  if (!userSponsorId) {
+    logger.warn('Invalid token: User Sponsor Id is missing');
+    return res.status(400).json({ error: 'Invalid token' });
+  }
+
+  logger.debug(`Request body: ${safeStringify(req.body)}`);
+
+  try {
+    const { error, listing } = await checkListingSponsorAuth(
+      userSponsorId,
+      id as string,
+    );
+    if (error) {
+      return res.status(error.status).json({ error: error.message });
+    }
+    if (listing.isPublished === false) {
+      logger.warn(`Listing with id=${id} is not published, cannot unpublish`);
+      return res
+        .status(400)
+        .json({ error: 'Listing is not published, cannot unpublish' });
+    }
+    if (listing.isActive === false) {
+      logger.warn(`Listing with id=${id} is not active, cannot unpublish`);
+      return res
+        .status(400)
+        .json({ error: 'Listing is not active, cannot unpublish' });
+    }
+    if (listing.isArchived === true) {
+      logger.warn(`Listing with id=${id} is archived, cannot unpublish`);
+      return res
+        .status(400)
+        .json({ error: 'Listing is archived, cannot unpublish' });
+    }
+    if (listing.status !== 'OPEN') {
+      logger.warn(`Listing with id=${id} is not open, cannot unpublish`);
+      return res
+        .status(400)
+        .json({ error: 'Listing is not open, cannot unpublish' });
+    }
+    if (listing.isWinnersAnnounced === true) {
+      logger.warn(
+        `Listing with id=${id} has announced winners, cannot unpublish`,
+      );
+      return res
+        .status(400)
+        .json({ error: 'Listing has announced winners, cannot unpublish' });
+    }
+    const result = await prisma.bounties.update({
+      where: { id },
+      data: { isPublished: false },
+    });
+    try {
+      await earncognitoClient.post(`/discord/listing-update`, {
+        listingId: id,
+        status: 'Unpublished',
+      });
+    } catch (err) {
+      logger.error('Discord Listing Unpublish Message Error', err);
+    }
+    logger.info(`Listing with id=${id} unpublished successfully`);
+    res.status(200).send(result);
+  } catch (error: any) {
+    logger.error(
+      `User ${userId} unable to unpublish a listing: ${safeStringify(error)}`,
+    );
+    return res.status(400).json({
+      error: error.message,
+      message: 'Error occurred while unpublishing a listing.',
+    });
+  }
+}
+
+export default withSponsorAuth(handler);
