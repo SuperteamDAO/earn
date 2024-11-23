82141144656df1af5e0e6b6c7b10f1649835c4b8
liushooter <shooter.hero@gmail.com>
2024-11-24 02:58:10 +0800

    chore: mobile banner

--- src/features/home/components/Banner.tsx
+++ src/features/home/components/Banner.tsx
@@ -2,7 +2,7 @@ import { Box, Show, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 import React from 'react';
 
-import MobileBanner from '@/public/assets/home/display/banner-mobile.webp';
+import MobileBanner from '@/public/assets/home/display/sign-banner.jpg';
 import DesktopBanner from '@/public/assets/home/display/sign-banner.jpg';
 
 export function HomeBanner() {
