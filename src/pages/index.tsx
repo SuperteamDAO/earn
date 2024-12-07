f39c2674d1544e57e52fed4e43fbe58068c3b56b
chee <gongkoking@gmail.com>
2024-12-08 01:14:20 +0800

    remove useless code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { Regions } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import type { GetServerSideProps } from 'next';
@@ -9,13 +9,11 @@ import { useEffect, useState } from 'react';
 import { CombinedRegions } from '@/constants/Superteam';
 import {
   homepageForYouListingsQuery,
-  homepageGrantsQuery,
   homepageListingsQuery,
 } from '@/features/home';
 import {
   getCombinedRegion,
   type Listing,
-  ListingSection,
   ListingTabs,
 } from '@/features/listings';
 import { Home } from '@/layouts/Home';
@@ -40,23 +38,11 @@ const InstallPWAModal = dynamic(
   { ssr: false },
 );
 
-const GrantsCard = dynamic(
-  () => import('@/features/grants').then((mod) => mod.GrantsCard),
-  { ssr: false },
-);
-
-const EmptySection = dynamic(
-  () =>
-    import('@/components/shared/EmptySection').then((mod) => mod.EmptySection),
-  { ssr: false },
-);
-
 export default function HomePage({
   listings,
   isAuth,
   userRegion,
   openForYouListings,
-  userGrantsRegion,
 }: Props) {
   const [combinedListings, setCombinedListings] = useState(listings);
   const [combinedForYouListings, setCombinedForYouListings] =
@@ -96,12 +82,6 @@ export default function HomePage({
     }),
   );
 
-  const { data: grants } = useQuery(
-    homepageGrantsQuery({
-      userRegion: userGrantsRegion,
-    }),
-  );
-
   useEffect(() => {
     if (reviewListings && completeListings) {
       setCombinedListings([
@@ -136,26 +116,6 @@ export default function HomePage({
           take={20}
           showViewAll
         />
-        {/* <ListingSection
-          type="grants"
-          title="资助"
-          sub="为建设者提供资金支持"
-          emoji="/assets/home/emojis/grants.webp"
-          showViewAll
-        >
-          {!grants?.length && (
-            <Flex align="center" justify="center" mt={8}>
-              <EmptySection
-                title="暂无资助可申请！"
-                message="订阅通知以便接收关于新资助项目的通知。"
-              />
-            </Flex>
-          )}
-          {grants &&
-            grants?.map((grant) => {
-              return <GrantsCard grant={grant} key={grant.id} />;
-            })}
-        </ListingSection> */}
       </Box>
     </Home>
   );
