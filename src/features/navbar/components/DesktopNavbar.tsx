5e482a47e7d9249bcace478506a78ab900da81e6
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2024-12-12 12:49:07 +0530

    ui: reduce left marging of blue dot in navbar

--- src/features/navbar/components/DesktopNavbar.tsx
+++ src/features/navbar/components/DesktopNavbar.tsx
@@ -115,7 +115,7 @@ export const DesktopNavbar = ({ onLoginOpen, onSearchOpen }: Props) => {
                 >
                   <Link href="/dashboard/listings">
                     Sponsor Dashboard
-                    <div className="ml-1.5 block h-1.5 w-1.5 rounded-full bg-[#38BDF8]" />
+                    <div className="block h-1.5 w-1.5 rounded-full bg-[#38BDF8]" />
                   </Link>
                 </Button>
               )}
@@ -136,7 +136,7 @@ export const DesktopNavbar = ({ onLoginOpen, onSearchOpen }: Props) => {
                   }}
                 >
                   Become a Sponsor
-                  <div className="ml-1.5 block h-1.5 w-1.5 rounded-full bg-[#38BDF8]" />
+                  <div className="block h-1.5 w-1.5 rounded-full bg-[#38BDF8]" />
                 </Button>
                 <Button
                   variant="ghost"
