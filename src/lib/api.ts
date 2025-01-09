433ebedb13332f32da63a8564a3663299675aebd
abhishek <abbhishekk27@gmail.com>
2025-01-09 18:41:27 +0530

    fix: revert interceptor logout on 401

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
-import { signOut } from 'next-auth/react';
 
 const api = axios.create();
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const currentPath = window.location.pathname;
-    localStorage.setItem('loginRedirectPath', currentPath);
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const currentPath = window.location.pathname;
+//     localStorage.setItem('loginRedirectPath', currentPath);
 
-    await signOut({
-      redirect: true,
-      callbackUrl: '/signin',
-    });
-    return Promise.reject(error);
-  },
-);
+//     await signOut({
+//       redirect: true,
+//       callbackUrl: '/signin',
+//     });
+//     return Promise.reject(error);
+//   },
+// );
 
 export { api };
