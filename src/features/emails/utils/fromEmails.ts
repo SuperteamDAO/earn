0bcc8f1b9a1d54f250d0e2a8171c8953377d9daf
abhishek <abbhishekk27@gmail.com>
2025-01-14 03:15:17 +0530

    chore: change name for pratik

--- src/features/emails/utils/fromEmails.ts
+++ src/features/emails/utils/fromEmails.ts
@@ -1,2 +1,2 @@
 export const replyToEmail = process.env.REPLY_TO_EMAIL;
-export const pratikEmail = `Pratik from Superteam <${process.env.PRATIK_EMAIL}>`;
+export const pratikEmail = `Pratik from Earn <${process.env.PRATIK_EMAIL}>`;
