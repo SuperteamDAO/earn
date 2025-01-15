19e3b4cb89dcd45918d32449c88919ed2f36c05b
abhishek <abbhishekk27@gmail.com>
2025-01-15 17:16:13 +0530

    fix: add solana wallets for mobile

--- src/context/SolanaWallet.tsx
+++ src/context/SolanaWallet.tsx
@@ -1,9 +1,12 @@
+import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { useMemo } from 'react';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
@@ -19,12 +22,21 @@ export function SolanaWalletProvider({
 
   const endpoint = useMemo(() => rpc, [network]);
 
+  const wallets = useMemo(
+    () => [
+      new BackpackWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    [],
+  );
+
   return (
     <ConnectionProvider
       endpoint={endpoint}
       config={{ commitment: 'confirmed' }}
     >
-      <WalletProvider wallets={[]} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
