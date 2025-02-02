a3de8861af97ca1f41eace29b49568584255a66b
abhishek <abbhishekk27@gmail.com>
2025-02-02 19:17:41 +0530

    feat: new default sorting logic for listings

--- src/pages/api/sponsor-dashboard/listings/index.ts
+++ src/pages/api/sponsor-dashboard/listings/index.ts
@@ -33,64 +33,76 @@ async function handler(req: NextApiRequestWithSponsor, res: NextApiResponse) {
   try {
     const data: BountyGrant[] = await prisma.$queryRawUnsafe(
       `
-      SELECT 
-        b.type as type,
-        b.id,
-        b.title,
-        b.slug,
-        b.token,
-        b.status,
-        b.deadline,
-        b.isPublished,
-        b.rewards,
-        b.rewardAmount,
-        b.totalWinnersSelected,
-        b.totalPaymentsMade,
-        b.isWinnersAnnounced,
-        b.maxRewardAsk,
-        b.minRewardAsk,
-        b.compensationType,
-        b.createdAt,
-        b.isFndnPaying,
-        NULL as airtableId,
-        CAST((SELECT COUNT(*) FROM Submission s WHERE s.listingId = b.id) AS SIGNED) as submissionCount
-      FROM Bounties b
-      WHERE b.isActive = true
-      AND b.isArchived = false
-      AND b.sponsorId = ?
-      AND b.status <> ?
-      
-      UNION ALL
-      
-      SELECT 
-        'grant' as type,
-        g.id,
-        g.title,
-        g.slug,
-        g.token,
-        g.status,
-        NULL as deadline,
-        g.isPublished,
-        NULL as rewards,
-        NULL as rewardAmount,
-        NULL as totalWinnersSelected,
-        g.totalPaid as totalPaymentsMade,
-        NULL as isWinnersAnnounced,
-        g.maxReward as maxRewardAsk,
-        g.minReward as minRewardAsk,
-        NULL as compensationType,
-        g.createdAt,
-        NULL as isFndnPaying,
-        g.airtableId,
-        CAST((SELECT COUNT(*) FROM GrantApplication ga WHERE ga.grantId = g.id) AS SIGNED) as submissionCount
-      FROM Grants g
-      WHERE g.isActive = true
-      AND g.isArchived = false
-      AND g.sponsorId = ?
-      AND g.status = ?
-      AND (g.airtableId IS NOT NULL OR g.isNative = true)
-      
-      ORDER BY createdAt DESC
+      WITH combined_data AS (
+        SELECT 
+          b.type as type,
+          b.id,
+          b.title,
+          b.slug,
+          b.token,
+          b.status,
+          b.deadline,
+          b.isPublished,
+          b.rewards,
+          b.rewardAmount,
+          b.totalWinnersSelected,
+          b.totalPaymentsMade,
+          b.isWinnersAnnounced,
+          b.maxRewardAsk,
+          b.minRewardAsk,
+          b.compensationType,
+          b.createdAt,
+          b.isFndnPaying,
+          NULL as airtableId,
+          CAST((SELECT COUNT(*) FROM Submission s WHERE s.listingId = b.id) AS SIGNED) as submissionCount
+        FROM Bounties b
+        WHERE b.isActive = true
+        AND b.isArchived = false
+        AND b.sponsorId = ?
+        AND b.status <> ?
+        
+        UNION ALL
+        
+        SELECT 
+          'grant' as type,
+          g.id,
+          g.title,
+          g.slug,
+          g.token,
+          g.status,
+          NULL as deadline,
+          g.isPublished,
+          NULL as rewards,
+          NULL as rewardAmount,
+          NULL as totalWinnersSelected,
+          g.totalPaid as totalPaymentsMade,
+          NULL as isWinnersAnnounced,
+          g.maxReward as maxRewardAsk,
+          g.minReward as minRewardAsk,
+          NULL as compensationType,
+          g.createdAt,
+          NULL as isFndnPaying,
+          g.airtableId,
+          CAST((SELECT COUNT(*) FROM GrantApplication ga WHERE ga.grantId = g.id) AS SIGNED) as submissionCount
+        FROM Grants g
+        WHERE g.isActive = true
+        AND g.isArchived = false
+        AND g.sponsorId = ?
+        AND g.status = ?
+        AND (g.airtableId IS NOT NULL OR g.isNative = true)
+      )
+      SELECT *
+      FROM combined_data
+      ORDER BY 
+        CASE 
+          WHEN deadline IS NULL THEN 1 
+          ELSE 0 
+        END,
+        CASE 
+          WHEN deadline IS NOT NULL THEN ABS(DATEDIFF(deadline, CURDATE()))
+          ELSE NULL 
+        END ASC,
+        createdAt DESC
     `,
       userSponsorId,
       status.CLOSED,
