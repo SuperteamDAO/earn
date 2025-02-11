e684502b751f864bde724d65d79b88cf1c9bfe8f
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2025-02-12 01:09:11 +0530

    fix: conversion popup listing popup usd value correctly

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,3 +1,6 @@
 export function roundToNearestThousand(number: number): number {
   return Math.round(number / 1000) * 1000;
 }
+export function roundToNearestTenth(number: number): number {
+  return Math.round(number / 10) * 10;
+}
