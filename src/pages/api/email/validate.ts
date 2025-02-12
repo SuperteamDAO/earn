73b8c511bc18a6ab53b8d1eb0118c08e7c3b7d7f
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-02-12 14:58:18 +0200

    feat: initial try to deploy

--- src/pages/api/email/validate.ts
+++ src/pages/api/email/validate.ts
@@ -22,18 +22,21 @@ export default async function handler(
   // }
 
   try {
-    const { data } = await axios.get(
-      `https://api.zerobounce.net/v2/validate?api_key=${process.env.ZEROBOUNCE_API_KEY}&email=${email}`,
-    );
-    console.log(data);
+    if (process.env.ZEROBOUNCE_API_KEY) {
+      const { data } = await axios.get(
+        `https://api.zerobounce.net/v2/validate?api_key=${process.env.ZEROBOUNCE_API_KEY}&email=${email}`,
+      );
+      const emailIsValid = data.status === 'valid';
+      const isRoleBased =
+        data.status === 'do_not_mail' && data.sub_status === 'role_based';
 
-    const emailIsValid = data.status === 'valid';
-    const isRoleBased =
-      data.status === 'do_not_mail' && data.sub_status === 'role_based';
+      const isValid = emailIsValid || isRoleBased;
 
-    const isValid = emailIsValid || isRoleBased;
-
-    return res.status(200).json({ isValid });
+      return res.status(200).json({ isValid });
+    } else {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return res.status(200).json({ isValid: emailRegex.test(email) });
+    }
   } catch (error) {
     console.error('Error validating email:', error);
     return res.status(500).json({ message: 'Error validating email' });
