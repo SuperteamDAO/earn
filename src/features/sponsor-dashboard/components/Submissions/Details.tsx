9bbf8e7a7378f671bd96e038159a206c7e2e3bf9
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2025-03-18 13:21:31 +0200

    feat: added submission comments to the sponsor dashboard (#64)
    @race-of-sloths

--- src/features/sponsor-dashboard/components/Submissions/Details.tsx
+++ src/features/sponsor-dashboard/components/Submissions/Details.tsx
@@ -1,6 +1,7 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
 import dayjs from 'dayjs';
 import { type Atom, useAtomValue } from 'jotai';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Checkbox } from '@/components/ui/checkbox';
 import { tokenList } from '@/constants/tokenList';
@@ -8,6 +9,7 @@ import { type SubmissionWithUser } from '@/interface/submission';
 import { cn } from '@/utils/cn';
 import { getURLSanitized } from '@/utils/getURLSanitized';
 
+import { Comments } from '@/features/comments/components/Comments';
 import { DescriptionUI } from '@/features/listings/components/ListingPage/DescriptionUI';
 import { type Listing } from '@/features/listings/types';
 
@@ -21,6 +23,8 @@ interface Props {
   atom?: Atom<SubmissionWithUser | undefined>;
 }
 
+type ActionTab = 'notes' | 'comments';
+
 export const Details = ({ bounty, modalView, atom }: Props) => {
   const selectedSubmission = useAtomValue(atom ?? selectedSubmissionAtom);
   const isProject = bounty?.type === 'project';
@@ -28,11 +32,13 @@ export const Details = ({ bounty, modalView, atom }: Props) => {
   const isUsdBased = bounty?.token === 'Any';
   const token = isUsdBased ? selectedSubmission?.token : bounty?.token;
   const tokenObject = tokenList.find((t) => t.tokenSymbol === token);
+  const [commentCount, setCommentCount] = useState(0);
+  const [activeTab, setActiveTab] = useState<ActionTab>('notes');
 
   return (
     <div
       className={cn(
-        'flex h-[32.6rem] w-full',
+        'flex h-[34rem] w-full',
         modalView ? 'mx-auto h-full max-w-3xl' : 'border-r border-slate-200',
       )}
     >
@@ -151,16 +157,61 @@ export const Details = ({ bounty, modalView, atom }: Props) => {
           isHtml
         />
       </div>
-      {!modalView && (
-        <div className="w-1/4 p-4">
-          {selectedSubmission && (
-            <Notes
-              key={selectedSubmission.id}
-              submissionId={selectedSubmission.id}
-              initialNotes={selectedSubmission.notes}
-              slug={bounty?.slug}
-            />
-          )}
+      {!modalView && selectedSubmission && (
+        <div className="w-1/3 border-l">
+          <Tabs
+            defaultValue="notes"
+            value={activeTab}
+            onValueChange={(tab) => setActiveTab(tab as ActionTab)}
+            className="w-full"
+          >
+            <TabsList className="grid h-auto w-full grid-cols-2 rounded-none">
+              <TabsTrigger
+                value="notes"
+                className={cn(
+                  'h-auto rounded-none border-b-2 px-4 py-2 text-muted-foreground data-[state=active]:border-brand-purple',
+                )}
+              >
+                Notes
+              </TabsTrigger>
+              <TabsTrigger
+                value="comments"
+                className={cn(
+                  'h-auto rounded-none border-b-2 px-4 py-2 text-muted-foreground data-[state=active]:border-brand-purple',
+                )}
+              >
+                Comments: {commentCount}
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="notes" className="p-0">
+              <div className="max-h-[32rem] overflow-y-auto px-4 py-2 scrollbar-thin scrollbar-track-slate-100 scrollbar-thumb-slate-300">
+                <Notes
+                  key={selectedSubmission?.id}
+                  submissionId={selectedSubmission?.id}
+                  initialNotes={selectedSubmission?.notes}
+                  slug={bounty?.slug}
+                />
+              </div>
+            </TabsContent>
+            <TabsContent value="comments" className="p-0">
+              <div className="max-h-[30rem] overflow-y-auto px-4 scrollbar-thin scrollbar-track-slate-100 scrollbar-thumb-slate-300">
+                <Comments
+                  isAnnounced={false}
+                  listingSlug={''}
+                  listingType={''}
+                  poc={undefined}
+                  sponsorId={undefined}
+                  isVerified={false}
+                  refId={selectedSubmission.id}
+                  refType={'SUBMISSION'}
+                  count={commentCount}
+                  setCount={setCommentCount}
+                  take={2}
+                />
+              </div>
+            </TabsContent>
+          </Tabs>
         </div>
       )}
     </div>
