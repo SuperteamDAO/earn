10f3860c73b0b91782c817230fbcf77a9082d22c
abhishek <abbhishekk27@gmail.com>
2025-03-27 22:43:08 +0530

    fix: airtable integration

--- src/features/grants/utils/addPaymentInfoToAirtable.ts
+++ src/features/grants/utils/addPaymentInfoToAirtable.ts
@@ -13,22 +13,34 @@ import { safeStringify } from '@/utils/safeStringify';
 interface GrantApplicationWithUserAndGrant extends GrantApplication {
   grant: {
     airtableId: string | null;
+    approverRecordId: string | null;
   };
   user: {
     email: string;
     location: string | null;
     kycName: string | null;
+    kycAddress: string | null;
+    kycDOB: string | null;
+    kycIDNumber: string | null;
+    kycIDType: string | null;
+    kycCountry: string | null;
   };
 }
 
 interface PaymentAirtableSchema {
   Name: string;
+  Address: string;
+  'Date of Birth': string;
+  'ID Number': string;
+  'ID Type': string;
+  'Country of Residence': string;
   Amount: number;
   'Wallet Address': string;
   Category: string[];
   'Purpose of Payment': string;
   Status: string;
   Region?: string[];
+  Approver: string[];
   earnApplicationId: string;
   earnTrancheId: string;
 }
@@ -40,6 +52,11 @@ export function grantApplicationToAirtable(
 ): PaymentAirtableSchema {
   return {
     Name: grantApplication.user.kycName || '',
+    Address: grantApplication.user.kycAddress || '',
+    'Date of Birth': grantApplication.user.kycDOB || '',
+    'ID Number': grantApplication.user.kycIDNumber || '',
+    'ID Type': grantApplication.user.kycIDType || '',
+    'Country of Residence': grantApplication.user.kycCountry || '',
     Amount: grantTranche.approvedAmount || 0,
     'Wallet Address': grantApplication.walletAddress || '',
     Category: ['recd0Kn3N4Ffhtwhd'], // Solana Grant
@@ -50,6 +67,7 @@ export function grantApplicationToAirtable(
           Region: [grantRegionId],
         }
       : {}),
+    Approver: [grantApplication.grant.approverRecordId || ''],
     earnApplicationId: grantApplication.id,
     earnTrancheId: grantTranche.id,
   };
@@ -98,7 +116,7 @@ export async function addPaymentInfoToAirtable(
       grantTranche,
     );
 
-    const airtablePayload = airtableInsert([{ fields: airtableData }]);
+    const airtablePayload = airtableInsert([{ fields: airtableData }], true);
 
     const response = await axios.post(
       paymentsAirtableURL,
