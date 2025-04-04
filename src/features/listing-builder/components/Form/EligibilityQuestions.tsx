917626fbf61a8eb9d5dd6374966e6653976940ab
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-04-04 18:04:04 +0300

    ui: bugfix

--- src/features/listing-builder/components/Form/EligibilityQuestions.tsx
+++ src/features/listing-builder/components/Form/EligibilityQuestions.tsx
@@ -377,7 +377,7 @@ export function EligibilityQuestions() {
                             </FormLabel>
                             <div className="relative flex rounded-md border ring-primary has-[:focus]:ring-1">
                               <div className="flex items-center justify-center border-r bg-slate-50">
-                                <GripVertical className="h-4 w-4 text-muted-foreground" />
+                                <GripVertical className="h-4 w-4 text-slate-400" />
                               </div>
                               <div className="flex w-full items-center">
                                 <FormField
