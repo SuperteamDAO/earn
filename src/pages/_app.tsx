705f3de5884558f3e5ed43633ff6b79fe48b9cde
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2025-04-09 13:48:12 +0530

    chore: disable auto capture events posthog and add grant application modal open / edit event

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,6 +54,7 @@ function MyApp({ Component, pageProps }: any) {
     if (!posthog.__loaded) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
         api_host: `${getURL()}ingest`,
+        autocapture: false,
         ui_host:
           process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
         loaded: (posthog) => {
