6a1d515395bdf9ba8003f53eeaa33018578eaecb
abhishek <abbhishekk27@gmail.com>
2025-04-22 00:27:13 +0530

    styles: update skeleton styles according to updated usermenu

--- src/features/navbar/components/DesktopNavbar.tsx
+++ src/features/navbar/components/DesktopNavbar.tsx
@@ -161,8 +161,8 @@ export const DesktopNavbar = ({
         <div className="flex items-center gap-4 py-1.5">
           {!ready && (
             <div className="flex items-center gap-2">
-              <Skeleton className="h-10 w-10 rounded-full" />
-              <Skeleton className="h-4 w-20" />
+              <Skeleton className="size-7 rounded-full" />
+              <Skeleton className="mr-4 h-3 w-20" />
             </div>
           )}
 
