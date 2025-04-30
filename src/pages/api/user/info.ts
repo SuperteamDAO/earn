3eb9ab7bc24230c0de71bd61c14152ad97eb0438
abhishek <abbhishekk27@gmail.com>
2025-05-01 02:40:44 +0530

    fix: adjust returned fields

--- src/pages/api/user/info.ts
+++ src/pages/api/user/info.ts
@@ -28,7 +28,6 @@ export default async function getAllUsers(
         lastName: true,
         skills: true,
         photo: true,
-        email: true,
         currentEmployer: true,
         location: true,
       },
