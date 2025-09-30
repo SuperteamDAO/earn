6e07271e9cb59a32bb08dede05b18cb1dd866d22
Jayesh Potlabattini <jayeshpotlabattini@gmail.com>
2025-09-30 14:45:20 +0530

    chore: unhide nav links

--- src/features/home/components/SideBar.tsx
+++ src/features/home/components/SideBar.tsx
@@ -15,6 +15,7 @@ import { totalsQuery } from '../queries/totals';
 import { HowItWorks } from './HowItWorks';
 import { RecentActivity } from './RecentActivity';
 import { RecentEarners } from './RecentEarners';
+import { SidebarBannerCypherpunk } from './SidebarBanner';
 import { SponsorBanner } from './SponsorBanner';
 import { TotalStats } from './TotalStats';
 
@@ -54,7 +55,7 @@ export const HomeSideBar = ({ type }: SideBarProps) => {
         {type === 'feed' && (
           <>
             <VibeCard />
-            {/* <SidebarBannerCypherpunk /> */}
+            <SidebarBannerCypherpunk />
             <LiveListings>
               <div className="flex items-center justify-between">
                 <span className="text-sm font-medium text-gray-400">
@@ -88,7 +89,7 @@ export const HomeSideBar = ({ type }: SideBarProps) => {
             </div>
 
             <HowItWorks />
-            {/* <SidebarBannerCypherpunk /> */}
+            <SidebarBannerCypherpunk />
             <RecentEarners earners={recentEarners} />
             <RecentActivity />
           </>
