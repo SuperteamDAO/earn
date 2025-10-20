61baacd4eabc6c203c4aa001c89488cef10a2466
abhishek <abbhishekk27@gmail.com>
2025-10-20 06:38:47 +0530

    .

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,9 +1,14 @@
-import type { NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  message: string;
-};
+import { createTranche } from '@/features/grants/utils/createTranche';
 
-export default function handler(res: NextApiResponse<Data>) {
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await createTranche({
+    isFirstTranche: true,
+    applicationId: '0a7bba36-c14d-4961-a648-9df916f87ffb',
+  });
   res.status(200).json({ message: "don't buy crypto, earn it" });
 }
