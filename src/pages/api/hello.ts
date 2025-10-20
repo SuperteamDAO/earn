7dc2d12e751af654f6f4f3375f46d64e3ee9184e
abhishek <abbhishekk27@gmail.com>
2025-10-20 06:46:28 +0530

    .

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,14 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { createTranche } from '@/features/grants/utils/createTranche';
-
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  await createTranche({
-    isFirstTranche: true,
-    applicationId: '0a7bba36-c14d-4961-a648-9df916f87ffb',
-  });
-  res.status(200).json({ message: "don't buy crypto, earn it" });
+  res.status(200).json({ message: "don't buy your crypto, earn it" });
 }
