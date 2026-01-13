592abb473eb0bf2d6318894b7e1596b0958520c2
abhishek <abhwshek@gmail.com>
2026-01-14 00:38:59 +0530

    fix: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,21 @@ export default function Document() {
     <Html data-theme="light" lang="en">
       <Head>
         <link rel="preconnect" href="https://res.cloudinary.com" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/earn/site.webmanifest" />
+        <link rel="apple-touch-icon" href="/earn/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/earn/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/earn/favicon-16x16.png"
+        />
+        <link rel="icon" href="/earn/favicon.ico" />
       </Head>
       <body>
         <Main />
