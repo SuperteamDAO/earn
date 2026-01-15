280a84fb28bb992831bfae880878ba695e3feb62
abhishek <abhwshek@gmail.com>
2026-01-16 05:01:37 +0530

    fix: st favicons

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -14,6 +14,7 @@ type IMetaProps = {
 const Meta = (props: IMetaProps) => {
   const router = useRouter();
   const ogImage = props.og ?? `${ASSET_URL}/og/og.png`;
+  const isEarnRoute = router.pathname.startsWith('/earn');
 
   return (
     <Head>
@@ -24,26 +25,43 @@ const Meta = (props: IMetaProps) => {
         content="width=device-width, initial-scale=1, maximum-scale=1"
         key="viewport"
       />
-      <link
-        rel="apple-touch-icon"
-        href={`${router.basePath}/apple-touch-icon.png`}
-        key="apple"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${router.basePath}/favicon-32x32.png`}
-        key="icon32"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${router.basePath}/favicon-16x16.png`}
-        key="icon16"
-      />
-      <link rel="icon" href={`${router.basePath}/favicon.ico`} key="favicon" />
+      {isEarnRoute ? (
+        <>
+          <link
+            rel="apple-touch-icon"
+            href={`${router.basePath}/apple-touch-icon.png`}
+            key="apple"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`${router.basePath}/favicon-32x32.png`}
+            key="icon32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`${router.basePath}/favicon-16x16.png`}
+            key="icon16"
+          />
+          <link
+            rel="icon"
+            href={`${router.basePath}/favicon.ico`}
+            key="favicon"
+          />
+        </>
+      ) : (
+        <>
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${router.basePath}/st-favicon.png`}
+            key="icon-png"
+          />
+        </>
+      )}
       <meta name="description" content={props.description} key="description" />
       {props.canonical && (
         <link rel="canonical" href={props.canonical} key="canonical" />
