490b36dca242a5c5a3a34d191eb714f3613bdb87
abhishek <abhwshek@gmail.com>
2026-03-19 14:21:20 +0530

    feat: next 16.2

--- next.config.ts
+++ next.config.ts
@@ -219,6 +219,9 @@ const nextConfig: NextConfig = {
     ];
   },
   skipTrailingSlashRedirect: true,
+  logging: {
+    browserToTerminal: true,
+  },
 };
 
 const combinedConfig = withAxiom(withPWA(nextConfig));
