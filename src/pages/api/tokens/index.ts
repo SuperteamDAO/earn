223b1ff0a3d181f591a9af352be532f61287e7f7
abhishek <abhwshek@gmail.com>
2026-03-26 19:36:18 +0530

    fix: image issue

--- src/pages/api/tokens/index.ts
+++ src/pages/api/tokens/index.ts
@@ -1,12 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import { getTokenList } from '@/server/tokenList';
+import { setCacheHeaders } from '@/utils/cacheControl';
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
+    setCacheHeaders(res, {
+      noStore: true,
+    });
+
     const tokens = await getTokenList();
     res.status(200).json({ tokens });
   } catch (error) {
