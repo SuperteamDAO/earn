da4834c0552461134095cafc82349e4583b9d733
Tanishq Parkar <tanishqparkar@gmail.com>
2026-05-18 14:54:26 +0530

    removed frontier from nav

--- src/features/hackathon/constants/hackathons.ts
+++ src/features/hackathon/constants/hackathons.ts
@@ -2,10 +2,4 @@ export const HACKATHONS: {
   label: string;
   slug: string;
   logo: string;
-}[] = [
-  {
-    label: 'Frontier',
-    slug: 'frontier',
-    logo: '/hackathon/frontier/logo.webp',
-  },
-];
+}[] = [];
